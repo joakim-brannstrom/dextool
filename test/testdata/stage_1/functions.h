c7f578860a89877247d6a9e65bf4eb8edc035ec3
Joakim Brannstrom <joakim.brannstrom@gmx.com>
2015-10-05 20:50:29 +0200

    added test case for C functions.

--- test/testdata/stage_1/functions.h
+++
@@ -1,5 +0,0 @@
-int foo (int a);
-void bar (int a, int b);
-void fooBar (void);
-int printf (char*, ...);
-int a (int out);
---
+++ test/testdata/stage_1/functions.hpp
@@ -0,0 +1,9 @@
+// Expecting definitions for the functions except printf.
+// printf should be generated as a skipped function.
+// A class with pure virtual member functions for each C function.
+
+int foo (int a);
+void bar (int a, int b);
+void fooBar (void);
+int printf (char*, ...);
+int a (int out);
