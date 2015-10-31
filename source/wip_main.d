fdede8614d7bdf35a5b6d45678276b15e42f8560
Joakim Brannstrom <joakim.brannstrom@gmx.com>
2015-10-31 22:03:21 +0100

    Basic visitor support in wip

--- source/wip_main.d
+++ source/wip_main.d
@@ -19,42 +19,88 @@
 module app;
 
 import std.conv;
-import std.stdio;
+import std.typecons : Nullable;
 import logger = std.experimental.logger;
 
 import clang.c.index;
 import clang.Cursor;
 
 import dsrcgen.cpp;
-import wip = generator.analyze.containers;
+import containers = generator.analyze.containers;
+import wip = generator.analyze.wip;
 import generator.clangcontext;
-import generator.analyzer : visitAst, IdStack, logNode, VisitNodeModule;
+import generator.analyzer : visitAst, IdStack, logNode, VisitNodeModule,
+    VisitNodeDepth;
 
-version (unittest) {
-    import tested;
+/** The constructor is disabled to force the class to be in a consistent state.
+ * static make to create ClassVisitor objects to avoid the unnecessary storage
+ * of a Cursor but still derive parameters from the Cursor.
+ */
+struct ClassVisitor {
+    import generator.analyze.containers : CppClass, CppClassName, VirtualType,
+        CppVirtualClass;
 
-    shared static this() {
-        import core.runtime;
+    /** By making ClassVisitor via this static function it is guaranteed that
+     * the same Cursor that the name and virtuality is derived from is the same
+     * that is used to visit the AST.
+     */
+    static ClassVisitor make(ref Cursor c) {
+        auto name = CppClassName(c.spelling);
+        auto isVirtual = CppVirtualClass(c.isVirtualBase ? VirtualType.Pure : VirtualType.No);
 
-        Runtime.moduleUnitTester = () => true;
+        auto r = ClassVisitor(name, isVirtual);
+        r.visit(c);
+        return r;
     }
+
+    @disable this();
+
+    private this(CppClassName name, CppVirtualClass virtual) {
+        data = CppClass(name, virtual);
+    }
+
+    void visit(ref Cursor c) {
+        if (!c.isDefinition) {
+            return;
+        }
+        wip.visitAst!(typeof(this))(c, this);
+    }
+
+    bool apply(ref Cursor child, ref Cursor parent) {
+        bool descend = true;
+
+        switch (child.kind) with (CXCursorKind) {
+        case CXCursor_ClassDecl:
+            break;
+        case CXCursor_Constructor:
+            break;
+        case CXCursor_Destructor:
+            break;
+        case CXCursor_CXXAccessSpecifier:
+            break;
+        default:
+            break;
+        }
+        return descend;
+    }
+
+    CppClass data;
 }
 
 struct EntryContext {
-    VisitNodeModule!CppModule visitor_stack;
-    alias visitor_stack this;
+    private VisitNodeDepth depth_;
+    alias depth_ this;
 
-    void traverse(Cursor cursor) {
-        visitAst!(typeof(this))(cursor, this);
+    void visit(Cursor cursor) {
+        wip.visitAst!(typeof(this))(cursor, this);
     }
 
-    bool apply(Cursor c) {
+    bool apply(ref Cursor c, ref Cursor parent) {
         bool descend = true;
         logNode(c, depth);
         switch (c.kind) with (CXCursorKind) {
         case CXCursor_ClassDecl:
-            if (c.isDefinition) {
-            }
+            descend = false;
             break;
 
         default:
@@ -63,6 +109,8 @@ struct EntryContext {
 
         return descend;
     }
+
+    containers.Root root;
 }
 
 int main(string[] args) {
@@ -81,7 +129,7 @@ int main(string[] args) {
     logger.infof("Testing '%s'", infile);
 
     EntryContext foo;
-    foo.traverse(file_ctx.cursor);
+    foo.visit(file_ctx.cursor);
 
     return 0;
 }
