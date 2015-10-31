e5a2c2f910959f89a13d181ecf9e46a23a650d5c
Joakim Brannstrom <joakim.brannstrom@gmx.com>
2015-10-31 22:00:45 +0100

    Added a visitor to wip module to allow further refactoring

--- source/generator/analyze/wip.d
+++ source/generator/analyze/wip.d
@@ -16,4 +16,31 @@
 /// You should have received a copy of the GNU General Public License
 /// along with this program; if not, write to the Free Software
 /// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
-//module generator.analyze.wip;
+module generator.analyze.wip;
+
+import clang.Cursor;
+import clang.Visitor : Visitor;
+
+void visitAst(VisitorType)(ref Cursor cursor, ref VisitorType v) {
+    static void helperVisitAst(VisitorType)(ref Cursor child, ref Cursor parent, ref VisitorType v) {
+        import std.traits;
+
+        static if (__traits(hasMember, VisitorType, "incr")) {
+            v.incr();
+        }
+        bool decend = v.apply(child, parent);
+
+        if (!child.isEmpty && decend) {
+            foreach (child_, parent_; Visitor(child)) {
+                helperVisitAst(child_, parent_, v);
+            }
+        }
+
+        static if (__traits(hasMember, VisitorType, "decr")) {
+            v.decr();
+        }
+    }
+
+    Cursor e = Cursor.empty(cursor.translationUnit);
+    helperVisitAst(cursor, e, v);
+}
