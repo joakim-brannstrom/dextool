9c2519c3d84f971ad4e004e6ca156059d9106ce3
Joakim Brannstrom <joakim.brannstrom@gmx.com>
2015-11-08 19:19:33 +0100

    split helper functions for testing to separate module

--- test/cstub_tests.d
+++ test/cstub_tests.d
@@ -5,103 +5,8 @@ License: $(LINK2 http://www.boost.org/LICENSE_1_0.txt, Boost Software License 1.
 Author: Joakim Brännström (joakim.brannstrom@gmx.com)
  */
 import scriptlike;
+import utils;
 import std.path : asAbsolutePath, asNormalizedPath;
-
-string OUTDIR = "outdata";
-string DEXTOOL;
-
-enum Color {
-    red,
-    green,
-    yellow,
-    cancel
-}
-
-struct GR {
-    Path gold;
-    Path result;
-}
-
-void print(T...)(Color c, T args) {
-    immutable string[] escCodes = ["\033[31;1m", "\033[32;1m", "\033[33;1m", "\033[0;;m"];
-
-    writeln(escCodes[c], args, escCodes[Color.cancel]);
-}
-
-void setupTestEnv() {
-    mkdirRecurse(OUTDIR);
-}
-
-void cleanTestEnv() {
-    auto range = dirEntries(OUTDIR, SpanMode.shallow);
-    range.each!(a => tryRemove(Path(a)));
-}
-
-void teardownTestEnv() {
-    tryRmdirRecurse(OUTDIR);
-}
-
-void compare(Path gold, Path result) {
-    Args args;
-    args ~= "diff";
-    args ~= "-u";
-    args ~= gold;
-    args ~= result;
-
-    writef("Comparing result: %s\t%s\n", gold, result);
-    run(args.data);
-}
-
-void runDextool(Path input, string[] pre_args, string[] flags) {
-    .scriptlikeEcho = true;
-    scope (exit)
-        .scriptlikeEcho = false;
-
-    Args args;
-    args ~= .DEXTOOL;
-    args ~= "ctestdouble";
-    args ~= pre_args;
-    args ~= "--out=" ~ .OUTDIR;
-    args ~= input.toString;
-
-    if (flags.length > 0) {
-        args ~= "--";
-        args ~= flags;
-    }
-
-    run(args.data);
-}
-
-void compareResult(T...)(T args) {
-    static assert(args.length >= 1);
-
-    foreach (a; args) {
-        if (existsAsFile(a.gold)) {
-            compare(a.gold, a.result);
-        }
-    }
-}
-
-void compileResult(Path input, Path main, string[] flags, string[] incls) {
-    .scriptlikeEcho = true;
-    scope (exit)
-        .scriptlikeEcho = false;
-
-    auto binout = Path(OUTDIR ~ "/binary");
-
-    Args args;
-    args ~= "g++";
-    args ~= flags;
-    args ~= "-g";
-    args ~= "-o" ~ binout.toString;
-    args ~= "-I" ~ .OUTDIR;
-    args ~= incls;
-    args ~= input;
-    args ~= main;
-
-    run(args.data);
-    run(binout.toString);
-}
 
 void stage1() {
     writeln("Stage 1");
@@ -287,10 +192,8 @@ int main(string[] args) {
         return 1;
     }
 
-    .OUTDIR = asNormalizedPath(asAbsolutePath(OUTDIR)).text;
-    .DEXTOOL = asNormalizedPath(asAbsolutePath(args[1])).text;
-    writeln("deXTool:\t", DEXTOOL);
-    writeln("tmp:\t\t", OUTDIR);
+    setOutdir("outdata");
+    setDextool(args[1]);
 
     // Setup and cleanup
     chdir(thisExePath.dirName);
