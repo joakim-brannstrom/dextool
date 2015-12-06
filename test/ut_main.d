3277aed10678cd70d328d4773527a42e235c907c
Joakim Brannstrom <joakim.brannstrom@gmx.com>
2015-12-06 13:12:09 +0100

    test: run unittests for all appropriate modules

--- test/ut_main.d
+++ test/ut_main.d
@@ -3,22 +3,33 @@ import std.stdio;
 import unit_threaded.runner;
 
 int main(string[] args) {
-    writeln(`Running unit tests from dirs ["translator", "test", "cpptooling"]`);
+    writeln(`Running unit tests`);
     //dfmt off
     return args.runTests!(
+                          "application.app_main",
+                          "application.app",
+                          "application.cpptestdouble",
+                          "application.ctestdouble",
+                          "application.utility",
                           "cpptooling.analyzer.type",
                           "cpptooling.analyzer.clang.context",
                           "cpptooling.analyzer.clang.type",
                           "cpptooling.analyzer.clang.visitor",
+                          "cpptooling.analyzer.type",
                           "cpptooling.data.representation",
                           "cpptooling.generator.stub.cstub",
+                          "cpptooling.generator.adapter",
+                          "cpptooling.generator.classes",
+                          "cpptooling.generator.cppvariant",
+                          "cpptooling.generator.func",
+                          "cpptooling.generator.gmock",
+                          "cpptooling.generator.includes",
                           "cpptooling.utility.clang",
                           "cpptooling.utility.conv",
                           "cpptooling.utility.range",
                           "cpptooling.utility.stack",
-                          "test.helpers",
-                          "application.app_main",
-                          "application.app"
+                          "cpptooling.utility.taggedalgebraic",
+                          "test.helpers"
                           );
     //dfmt on
 }
