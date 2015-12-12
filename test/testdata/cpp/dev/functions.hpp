c7cba4c7e3ceff1131fbd40839cb592e99be2060
Joakim Brannstrom <joakim.brannstrom@gmx.com>
2015-12-12 23:56:58 +0100

    test: updated for identity position in ptr and ref types.
    the trailing whitespace after const is considered a bug.

--- test/testdata/cpp/dev/functions.hpp
+++ test/testdata/cpp/dev/functions.hpp
@@ -45,7 +45,7 @@ int& func_return_ref();
 void func_ref_many(int& a, char& b);
 void func_array(int a[10]);
 void func_ref_ptr(int*& a);
-// void func_ref_array(int (&a)[10]);
+void func_ref_array(int (&a)[10]);
 
 } // NS: ns
 #endif // FUNCTIONS_H
