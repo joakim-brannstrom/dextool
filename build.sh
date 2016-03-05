9e2006aeefcd6c2d508d3dc88377e1914ec429ab
Joakim Brannstrom <joakim.brannstrom@gmx.com>
2016-03-05 18:00:42 +0100

    Add makefile to allow building deXtoo without dub

--- build.sh
+++ build.sh
@@ -7,10 +7,12 @@ test ! -d build && mkdir build
 source ./config.sh
 
 if [[ $# -eq 0 ]]; then
-    dub build -c release -b release
     dub build -c debug
     dub build -c profile -b profile
     dub build -c devtool -b debug
+
+    # release build is always from makefile
+    make dmd
 else
     dub $@
 fi
