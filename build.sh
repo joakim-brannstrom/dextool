d14a7450b2fba14d67a7f3af9e1894e9562dcf54
Joakim Brannstrom <joakim.brannstrom@gmx.com>
2016-03-05 18:00:08 +0100

    Split configuration to allow the config to be ran separately from the build

--- build.sh
+++ build.sh
@@ -4,24 +4,7 @@ set -e
 # create build if missing
 test ! -d build && mkdir build
 
-if [[ -n "$LFLAGS" ]]; then
-    echo "Using user env flag"
-elif [[ -d "/usr/lib/llvm-3.7/lib" ]]; then
-    export LFLAGS="-L/usr/lib/llvm-3.7/lib"
-elif [[ -d "/usr/lib/llvm-3.6/lib" ]]; then
-    export LFLAGS="-L/usr/lib/llvm-3.6/lib"
-elif [[ -d "/usr/lib64/llvm" ]]; then
-    export LFLAGS="-L/usr/lib64/llvm"
-fi
-
-if [[ -n "$LFLAGS" ]]; then
-    echo "LFLAGS=$LFLAGS"
-else
-    echo "You must export the environment variable LFLAGS with suitable linker flags to allow dmd to find libclang.so.1"
-    echo "Example:"
-    echo 'export LFLAGS="-L/usr/lib/llvm-3.6/lib"'
-    exit 1
-fi
+source ./config.sh
 
 if [[ $# -eq 0 ]]; then
     dub build -c release -b release
