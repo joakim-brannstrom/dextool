fe324958b10b2b50af347c4de8d5f104ef3de1f7
Joakim Brannstrom <joakim.brannstrom@gmx.com>
2016-03-06 21:31:09 +0100

    test: convert C golden files to using D unittest blocks
    Improves:
     - Visibility of existing tests.
     - Configuration of input, output etc.
     - Allows reuse of unit-threaded functionality.
     - Store each tests output for later analyze.

--- test/cstub_tests.d
+++ test/cstub_tests.d
@@ -1,214 +1,286 @@
 // Written in the D programming language.
 /**
-Date: 2015, Joakim Brännström
+Copyright: Copyright (c) 2015-2016, Joakim Brännström. All rights reserved.
 License: $(LINK2 http://www.boost.org/LICENSE_1_0.txt, Boost Software License 1.0)
 Author: Joakim Brännström (joakim.brannstrom@gmx.com)
  */
+module ctestdouble_tests;
+
 import scriptlike;
 import utils;
-import std.path : asAbsolutePath, asNormalizedPath;
+import std.typecons : Flag, Yes, No;
 
-void stage1() {
-    writeln("Stage 1");
-    auto root = Path("testdata/cstub/stage_1");
-    auto files = dirEntries(root, "*.{h,hpp}", SpanMode.shallow);
+import unit_threaded.runner;
+import unit_threaded : Name, shouldEqual, ShouldFail;
 
-    const auto flags = compilerFlags();
+enum globalTestdir = "c_tests";
 
-    foreach (f; files) {
-        auto input_ext = Path(f);
-        scope (failure)
-            testEnv.save(input_ext.baseName.toString);
+struct TestParams {
+    Flag!"skipCompare" skipCompare;
+    Flag!"skipCompile" skipCompile;
 
-        auto out_hdr = testEnv.outdir ~ "test_double.hpp";
-        auto out_impl = testEnv.outdir ~ "test_double.cpp";
-        auto out_global = testEnv.outdir ~ "test_double_global.cpp";
-        auto out_gmock = testEnv.outdir ~ "test_double_gmock.hpp";
+    Path root;
+    Path input_ext;
+    Path out_hdr;
+    Path out_impl;
+    Path out_global;
+    Path out_gmock;
 
-        printStatus(Status.Run, input_ext);
-        auto params = ["--DRT-gcopt=profile:1", "ctestdouble", "--debug"];
-        switch (input_ext.baseName.toString) {
-        case "class_func.hpp":
-            runDextool(input_ext, params, ["-xc++", "-DAND_A_DEFINE"]);
-            break;
-        case "param_main.h":
-            runDextool(input_ext, params ~ ["--main=Stub", "--main-fname=stub"], []);
-            out_hdr = out_hdr.up ~ "stub.hpp";
-            out_impl = out_impl.up ~ "stub.cpp";
-            break;
-        case "test_include_stdlib.h":
-            runDextool(input_ext, params, ["-nostdinc"]);
-            break;
-        case "param_gmock.h":
-            runDextool(input_ext, params ~ ["--gmock"], ["-nostdinc"]);
-            break;
+    // dextool parameters;
+    string[] dexParams;
+    string[] dexFlags;
 
-        default:
-            runDextool(input_ext, params, []);
-        }
+    // Compiler flags
+    string[] compileFlags;
+    string[] compileIncls;
 
-        println(Color.yellow, "Comparing");
-        auto input = input_ext.stripExtension;
-        compareResult(GR(input ~ Ext(".hpp.ref"), out_hdr),
-                GR(input ~ Ext(".cpp.ref"), out_impl), GR(Path(input.toString ~ "_global.cpp.ref"),
-                    out_global), GR(Path(input.toString ~ "_gmock.hpp.ref"), out_gmock));
+    Path mainf;
+}
 
-        println(Color.yellow, "Compiling");
-        auto incls = ["-I" ~ input_ext.dirName.absolutePath.toString];
-        auto mainf = Path("testdata/cstub/main1.cpp").absolutePath;
-        switch (input_ext.baseName.toString) {
-        case "bug_wchar.h":
-        case "test_include_stdlibs.h":
-            // skip compiling, stdarg.h etc do not exist on all platforms
-            break;
-        case "param_gmock.h":
-            compileResult(out_impl, mainf, flags ~ ["-DTEST_INCLUDE", "-DTEST_FUNC_PTR"], incls);
-            break;
-        case "param_main.h":
-            compileResult(out_impl, mainf, flags.dup, incls);
-            break;
-        case "variables.h":
-            compileResult(out_impl, mainf, flags.dup, incls);
-            break;
-        case "const.h":
-            compileResult(out_impl, mainf, flags ~ ["-DTEST_INCLUDE", "-DTEST_CONST"], incls);
-            break;
-        case "function_pointers.h":
-            compileResult(out_impl, mainf, flags ~ ["-DTEST_INCLUDE", "-DTEST_FUNC_PTR"], incls);
-            break;
-        case "arrays.h":
-            compileResult(out_impl, mainf, flags ~ ["-DTEST_INCLUDE", "-DTEST_ARRAY"], incls);
-            break;
+TestParams genTestParams(string f, const ref TestEnv testEnv) {
+    TestParams p;
 
-        default:
-            compileResult(out_impl, mainf, flags ~ ["-DTEST_INCLUDE"], incls);
-        }
+    p.root = Path("testdata/cstub").absolutePath;
+    p.input_ext = p.root ~ Path(f);
 
-        printStatus(Status.Ok, input_ext);
-        testEnv.clean();
+    p.out_hdr = testEnv.outdir ~ "test_double.hpp";
+    p.out_impl = testEnv.outdir ~ "test_double.cpp";
+    p.out_global = testEnv.outdir ~ "test_double_global.cpp";
+    p.out_gmock = testEnv.outdir ~ "test_double_gmock.hpp";
+
+    p.dexParams = ["--DRT-gcopt=profile:1", "ctestdouble", "--debug"];
+    p.dexFlags = [];
+
+    p.compileFlags = compilerFlags();
+    p.compileIncls = ["-I" ~ p.input_ext.dirName.toString];
+
+    p.mainf = p.root ~ Path("main1.cpp");
+
+    return p;
+}
+
+void runTestFile(const ref TestParams p, ref TestEnv testEnv) {
+    testEnv.echo("Input:%s", p.input_ext.toRawString);
+    runDextool(p.input_ext, testEnv, p.dexParams, p.dexFlags);
+
+    if (!p.skipCompare) {
+        testEnv.echo("Comparing");
+        auto input = p.input_ext.stripExtension;
+        // dfmt off
+        compareResult(
+                      GR(input ~ Ext(".hpp.ref"), p.out_hdr),
+                      GR(input ~ Ext(".cpp.ref"), p.out_impl),
+                      GR(Path(input.toString ~ "_global.cpp.ref"), p.out_global),
+                      GR(Path(input.toString ~ "_gmock.hpp.ref"), p.out_gmock));
+        // dfmt on
+    }
+
+    if (!p.skipCompile) {
+        testEnv.echo("Compiling");
+        compileResult(p.out_impl, p.mainf, testEnv, p.compileFlags, p.compileIncls);
     }
 }
 
-void stage2() {
-    writeln("Stage 2");
+@Name("Should be correct declarations of arrays")
+unittest {
+    mixin(EnvSetup(globalTestdir));
+    auto p = genTestParams("stage_1/arrays.h", testEnv);
+    p.compileFlags ~= ["-DTEST_ARRAY", "-DTEST_INCLUDE"];
+    runTestFile(p, testEnv);
+}
 
-    auto root = Path("testdata/cstub/stage_2");
-    auto files = dirEntries(root, "*.{h,hpp}", SpanMode.shallow);
+@Name("Should ignore C++ code")
+unittest {
+    mixin(EnvSetup(globalTestdir));
+    auto p = genTestParams("stage_1/class_func.hpp", testEnv);
+    p.dexFlags = ["-xc++", "-DAND_A_DEFINE"];
+    p.compileFlags ~= "-DTEST_INCLUDE";
+    runTestFile(p, testEnv);
+}
 
-    const auto flags = compilerFlags();
+@Name("Should be global constants with defines to allow initialization")
+unittest {
+    mixin(EnvSetup(globalTestdir));
+    auto p = genTestParams("stage_1/const.h", testEnv);
+    p.compileFlags ~= ["-DTEST_CONST", "-DTEST_INCLUDE"];
+    runTestFile(p, testEnv);
+}
 
-    foreach (f; files) {
-        auto input_ext = Path(f);
-        scope (failure)
-            testEnv.save(input_ext.baseName.toString);
+@Name("Should be plain function pointers or implementations")
+unittest {
+    //TODO split the test in two, "global func pointers"/"use typedef func prototype for declaration"
+    mixin(EnvSetup(globalTestdir));
+    auto p = genTestParams("stage_1/function_pointers.h", testEnv);
+    p.compileFlags ~= "-DTEST_FUNC_PTR";
+    runTestFile(p, testEnv);
+}
 
-        auto out_hdr = testEnv.outdir ~ "test_double.hpp";
-        auto out_impl = testEnv.outdir ~ "test_double.cpp";
-        auto out_global = testEnv.outdir ~ "test_double_global.cpp";
-        auto out_gmock = testEnv.outdir ~ "test_double_gmock.hpp";
+@Name("Should be implementations of C functions")
+unittest {
+    mixin(EnvSetup(globalTestdir));
+    auto p = genTestParams("stage_1/functions.h", testEnv);
+    p.compileFlags ~= "-DTEST_INCLUDE";
+    runTestFile(p, testEnv);
+}
 
-        printStatus(Status.Run, input_ext);
-        auto params = ["--DRT-gcopt=profile:1", "ctestdouble", "--debug"];
-        auto incls = ["-I" ~ (root ~ "include").toString];
-        switch (input_ext.baseName.toString) {
-        case "no_overwrite.h":
-            copy(root ~ "no_overwrite_pre_includes.hpp",
-                    testEnv.outdir ~ "test_double_pre_includes.hpp");
-            copy(root ~ "no_overwrite_post_includes.hpp",
-                    testEnv.outdir ~ "test_double_post_includes.hpp");
-            runDextool(input_ext, params ~ ["--gen-pre-incl",
-                    "--gen-post-incl"], incls ~ ["-DPRE_INCLUDES"]);
-            break;
-        case "no_overwrite_post_includes.hpp":
-        case "no_overwrite_pre_includes.hpp":
-            continue;
+@Name(
+        "Should be a google mock of the interface used as callback from the C function implementations")
+unittest {
+    mixin(EnvSetup(globalTestdir));
+    auto p = genTestParams("stage_1/param_gmock.h", testEnv);
+    p.dexParams ~= "--gmock";
+    p.dexFlags ~= "-nostdinc";
+    p.compileFlags ~= ["-DTEST_INCLUDE", "-DTEST_FUNC_PTR"];
+    runTestFile(p, testEnv);
+}
 
-        case "param_exclude_many_files.h":
-            runDextool(input_ext, params ~ ["--file-exclude=.*/" ~ input_ext.baseName.toString,
-                    `--file-exclude='.*/include/b\.[h,c]'`], incls);
-            break;
-        case "param_exclude_match_all.h":
-            runDextool(input_ext, params ~ ["--file-exclude=.*/param_exclude_match_all.*",
-                    `--file-exclude='.*/include/b\.c'`], incls);
-            break;
-        case "param_exclude_one_file.h":
-            runDextool(input_ext,
-                    params ~ ["--file-exclude=.*/" ~ input_ext.baseName.toString], incls);
-            break;
-        case "param_gen_pre_post_include.h":
-            runDextool(input_ext, params ~ ["--gen-pre-incl", "--gen-post-incl"], incls);
-            break;
-        case "param_include.h":
-            runDextool(input_ext, params ~ ["--td-include=b.h", "--td-include=stdio.h"], incls);
-            break;
-        case "param_restrict.h":
-            runDextool(input_ext, params ~ ["--file-restrict=.*/" ~ input_ext.baseName.toString,
-                    "--file-restrict=.*/include/b.h"], incls);
-            break;
+@Name("Interface and adapter should be affected by parameter --main=X")
+unittest {
+    mixin(EnvSetup(globalTestdir));
+    auto p = genTestParams("stage_1/param_main.h", testEnv);
+    p.dexParams ~= ["--main=Stub", "--main-fname=stub"];
+    p.out_hdr = p.out_hdr.up ~ "stub.hpp";
+    p.out_impl = p.out_impl.up ~ "stub.cpp";
+    p.compileFlags = [];
+    runTestFile(p, testEnv);
+}
 
-        default:
-            runDextool(input_ext, params, incls);
-        }
+@Name("Should ignore the structs")
+unittest {
+    mixin(EnvSetup(globalTestdir));
+    auto p = genTestParams("stage_1/structs.h", testEnv);
+    p.compileFlags ~= "-DTEST_INCLUDE";
+    runTestFile(p, testEnv);
+}
 
-        println(Color.yellow, "Comparing");
-        auto input = input_ext.stripExtension;
-        switch (input_ext.baseName.toString) {
-        case "no_overwrite.h":
-            compareResult(GR(input.up ~ "no_overwrite_pre_includes.hpp",
-                    testEnv.outdir ~ "test_double_pre_includes.hpp"),
-                    GR(input.up ~ "no_overwrite_post_includes.hpp",
-                        testEnv.outdir ~ "test_double_post_includes.hpp"));
-            break;
-        case "param_gen_pre_post_include.h":
-            compareResult(GR(input ~ Ext(".hpp.ref"), out_hdr), GR(input ~ Ext(".cpp.ref"),
-                    out_impl), GR(input.up ~ "param_gen_pre_includes.hpp.ref",
-                    testEnv.outdir ~ "test_double_pre_includes.hpp"),
-                    GR(input.up ~ "param_gen_post_includes.hpp.ref",
-                        testEnv.outdir ~ "test_double_post_includes.hpp"));
-            break;
+@Name("Should use the internal headers in the binary even if -nostdinc is one of the compile flags")
+unittest {
+    mixin(EnvSetup(globalTestdir));
+    auto p = genTestParams("stage_1/test_include_stdlibs.h", testEnv);
+    // skip compiling, stdarg.h etc do not exist on all platforms
+    p.skipCompile = Yes.skipCompile;
+    runTestFile(p, testEnv);
+}
 
-        default:
-            compareResult(GR(input ~ Ext(".hpp.ref"), out_hdr),
-                    GR(input ~ Ext(".cpp.ref"), out_impl), GR(Path(input.toString ~ "_global.cpp.ref"),
-                        out_global), GR(Path(input.toString ~ "_gmock.hpp.ref"), out_gmock));
-        }
+@Name("Should ignore union declarations")
+unittest {
+    mixin(EnvSetup(globalTestdir));
+    auto p = genTestParams("stage_1/unions.h", testEnv);
+    p.compileFlags ~= "-DTEST_INCLUDE";
+    runTestFile(p, testEnv);
+}
 
-        println(Color.yellow, "Compiling");
-        auto mainf = Path("testdata/cstub/main1.cpp");
-        incls ~= "-I" ~ input_ext.dirName.toString;
-        switch (input_ext.baseName.toString) {
-        default:
-            compileResult(out_impl, mainf, flags ~ ["-DTEST_INCLUDE"], incls);
-        }
+@Name("Should be definitions of global variables for those that are extern")
+unittest {
+    mixin(EnvSetup(globalTestdir));
+    auto p = genTestParams("stage_1/variables.h", testEnv);
+    runTestFile(p, testEnv);
+}
 
-        printStatus(Status.Ok, input_ext);
-        testEnv.clean();
-    }
+@Name("Should not overwrite an existing X_pre_includes or X_post_includes.hpp")
+unittest {
+    mixin(EnvSetup(globalTestdir));
+    auto p = genTestParams("stage_2/no_overwrite.h", testEnv);
+    p.compileIncls ~= "-I" ~ (p.root ~ "stage_2/include").toString;
+    p.dexParams ~= ["--gen-pre-incl", "--gen-post-incl"];
+    p.dexFlags ~= "-DPRE_INCLUDES";
+    p.compileFlags ~= "-DPRE_INCLUDES";
+
+    copy(p.root ~ "stage_2/no_overwrite_pre_includes.hpp",
+            testEnv.outdir ~ "test_double_pre_includes.hpp");
+    copy(p.root ~ "stage_2/no_overwrite_post_includes.hpp",
+            testEnv.outdir ~ "test_double_post_includes.hpp");
+
+    runTestFile(p, testEnv);
+}
+
+@Name("Should exclude many files from the generated test double")
+unittest {
+    mixin(EnvSetup(globalTestdir));
+    auto p = genTestParams("stage_2/param_exclude_many_files.h", testEnv);
+    p.dexParams ~= ["--file-exclude=.*/" ~ p.input_ext.baseName.toString,
+        `--file-exclude='.*/include/b\.[h,c]'`];
+    p.compileIncls ~= "-I" ~ (p.root ~ "stage_2/include").toString;
+
+    p.dexFlags = p.compileIncls;
+
+    runTestFile(p, testEnv);
+}
+
+@Name("Should exclude both main input file and all symbols from b*")
+unittest {
+    mixin(EnvSetup(globalTestdir));
+    auto p = genTestParams("stage_2/param_exclude_match_all.h", testEnv);
+    p.dexParams ~= ["--file-exclude=.*/param_exclude_match_all.*",
+        `--file-exclude='.*/include/b\.c'`];
+    p.compileIncls ~= "-I" ~ (p.root ~ "stage_2/include").toString;
+
+    p.dexFlags = p.compileIncls;
+
+    runTestFile(p, testEnv);
+}
+
+@Name("Should exclude this file from generation.")
+unittest {
+    mixin(EnvSetup(globalTestdir));
+    auto p = genTestParams("stage_2/param_exclude_one_file.h", testEnv);
+    p.compileIncls ~= "-I" ~ (p.root ~ "stage_2/include").toString;
+    p.dexParams ~= "--file-exclude=.*/" ~ p.input_ext.baseName.toString;
+
+    p.dexFlags = p.compileIncls;
+
+    runTestFile(p, testEnv);
+}
+
+@Name("Should generate pre and post includes")
+unittest {
+    mixin(EnvSetup(globalTestdir));
+    auto p = genTestParams("stage_2/param_gen_pre_post_include.h", testEnv);
+    p.dexParams ~= ["--gen-pre-incl", "--gen-post-incl"];
+    p.compileIncls ~= "-I" ~ (p.root ~ "stage_2/include").toString;
+    p.skipCompare = Yes.skipCompare;
+
+    runTestFile(p, testEnv);
+
+    // dfmt off
+    testEnv.echo("Comparing");
+    auto input = p.input_ext.stripExtension;
+    compareResult(GR(input ~ Ext(".hpp.ref"), p.out_hdr),
+                  GR(input ~ Ext(".cpp.ref"), p.out_impl),
+                  GR(input.up ~ "param_gen_pre_includes.hpp.ref", testEnv.outdir ~ "test_double_pre_includes.hpp"),
+                  GR(input.up ~ "param_gen_post_includes.hpp.ref", testEnv.outdir ~ "test_double_post_includes.hpp"));
+    // dfmt on
+}
+
+@Name("Should be all from this and b with the extra include stdio.h")
+unittest {
+    mixin(EnvSetup(globalTestdir));
+    auto p = genTestParams("stage_2/param_include.h", testEnv);
+    p.dexParams ~= ["--td-include=b.h", "--td-include=stdio.h"];
+    p.compileIncls ~= "-I" ~ (p.root ~ "stage_2/include").toString;
+
+    p.dexFlags = p.compileIncls;
+
+    runTestFile(p, testEnv);
+}
+
+@Name("Should only be signatures from this file and b.h in the generated stub")
+unittest {
+    mixin(EnvSetup(globalTestdir));
+    auto p = genTestParams("stage_2/param_restrict.h", testEnv);
+    p.dexParams ~= ["--file-restrict=.*/" ~ p.input_ext.baseName.toString,
+        "--file-restrict=.*/include/b.h"];
+    p.compileIncls ~= "-I" ~ (p.root ~ "stage_2/include").toString;
+
+    p.dexFlags = p.compileIncls;
+
+    runTestFile(p, testEnv);
 }
 
 int main(string[] args) {
-    if (args.length <= 1) {
-        writef("Usage: %s <path-to-dextool>\n", args[0]);
-        return 1;
-    }
+    //import unit_threaded : enableStackTrace;
+    //
+    //enableStackTrace();
 
-    testEnv = TestEnv("outdir", "c_fail_log", args[1]);
-
-    // Setup and cleanup
-    chdir(thisExePath.dirName);
-    scope (exit)
-        testEnv.teardown();
-    testEnv.setup();
-
-    // start testing
-    try {
-        stage1();
-        stage2();
-    }
-    catch (ErrorLevelException ex) {
-        printStatus(Status.Fail, ex.msg);
-        return 1;
-    }
-
-    return 0;
+    return args.runTests!("ctestdouble_tests");
 }
