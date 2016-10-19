ae8123d03f64481478c2fc3274010f1d0204267c
Joakim Brannstrom <joakim.brannstrom@gmx.com>
2016-10-19 20:36:25 +0200

    The backtracked scope must be traversed in reverse order to
    reconstruct the scope for a textual representation.

--- test/testdata/shared_input/cpp/functions_in_ns.hpp
+++ test/testdata/shared_input/cpp/functions_in_ns.hpp
@@ -16,6 +16,7 @@ void func_ref_array(int (&a)[10]);
 // function signature.
 // This is only a problem for C++ code.
 namespace Scope {
+namespace Inner {
 
 typedef enum {
     Enum_0,
@@ -25,25 +26,28 @@ typedef enum {
 typedef struct {
 } Struct;
 
+} // NS: Inner
 } // NS: Scope
 
 namespace ns_using_scope {
+namespace ns_using_inner {
 
-// function using types from Scope in the function signature
-void enum_named_arg(Scope::Enum e0);
-void enum_unnamed_arg(Scope::Enum);
-void enum_ptr_arg(Scope::Enum* e0);
-void enum_ref_arg(Scope::Enum& e0);
-Scope::Enum enum_rval();
-Scope::Enum* enum_ptr_rval();
-Scope::Enum& enum_ref_rval();
+// function using types from Scope::Inner in the function signature
+void enum_named_arg(Scope::Inner::Enum e0);
+void enum_unnamed_arg(Scope::Inner::Enum);
+void enum_ptr_arg(Scope::Inner::Enum* e0);
+void enum_ref_arg(Scope::Inner::Enum& e0);
+Scope::Inner::Enum enum_rval();
+Scope::Inner::Enum* enum_ptr_rval();
+Scope::Inner::Enum& enum_ref_rval();
 
-void struct_one_named_arg(Scope::Struct e0);
-void struct_unnamed_arg(Scope::Struct);
-void struct_ptr_arg(Scope::Struct* e0);
-void struct_ref_arg(Scope::Struct& e0);
-Scope::Struct struct_rval();
-Scope::Struct* struct_ptr_rval();
-Scope::Struct& struct_ref_rval();
+void struct_one_named_arg(Scope::Inner::Struct e0);
+void struct_unnamed_arg(Scope::Inner::Struct);
+void struct_ptr_arg(Scope::Inner::Struct* e0);
+void struct_ref_arg(Scope::Inner::Struct& e0);
+Scope::Inner::Struct struct_rval();
+Scope::Inner::Struct* struct_ptr_rval();
+Scope::Inner::Struct& struct_ref_rval();
 
+} // NS: ns_using_inner
 } // NS: ns_using_scope
