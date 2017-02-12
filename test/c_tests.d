ad6b017bac02767065cca6e608674e2d05329997
Joakim Brannstrom <joakim.brannstrom@gmx.com>
2017-02-13 00:14:07 +0100

    test: renamed tests for c test double

--- test/c_tests.d
+++ test/c_tests.d
@@ -3,7 +3,7 @@ Copyright: Copyright (c) 2015-2017, Joakim Brännström. All rights reserved.
 License: $(LINK2 http://www.boost.org/LICENSE_1_0.txt, Boost Software License 1.0)
 Author: Joakim Brännström (joakim.brannstrom@gmx.com)
  */
-module cstub_tests;
+module c_tests;
 
 import scriptlike;
 import utils;
