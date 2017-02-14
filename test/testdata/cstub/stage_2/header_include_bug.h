92cf293a7a42b196a9b1aa7169a5bf132b401494
Joakim Brannstrom <joakim.brannstrom@gmx.com>
2017-02-14 16:23:03 +0100

    test: add test that exhibit the include bug when combining multiple
    --in with --file-exclude

---
+++ test/testdata/cstub/stage_2/header_include_bug.h
@@ -0,0 +1,6 @@
+// Test how --exclude and multiple --in interact to generate the include's
+// There is a bug where --in override include's that are needed.
+// expecting includes of b.h and c.h
+
+#include "b.h"
+
