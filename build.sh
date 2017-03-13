8b966cf83fc7c017cbb4f5b3ed5a1935ae8a4c92
Joakim Brannstrom <joakim.brannstrom@gmx.com>
2017-03-14 00:15:21 +0100

    Update build stuff

--- build.sh
+++ build.sh
@@ -14,6 +14,18 @@ fi
 if [[ $# -eq 0 ]]; then
     dub build $COMPILER -c application -b release
     dub build $COMPILER -c debug -b debug
+elif [[ ( $# -eq 1 ) && ( $1 == "debug_build" ) ]]; then
+    dub build -c debug -b debug
+    dub build -c plugin_ctestdouble_debug -b debug
+    dub build -c plugin_cpptestdouble_debug -b debug
+    dub build -c plugin_graphml_debug -b debug
+    dub build -c plugin_uml_debug -b debug
+elif [[ ( $# -eq 1 ) && ( $1 == "release_build" ) ]]; then
+    dub build -c application
+    dub build -c plugin_ctestdouble
+    dub build -c plugin_cpptestdouble
+    dub build -c plugin_graphml
+    dub build -c plugin_uml
 elif [[ ( $# -eq 1 ) && ( $1 == "make" ) ]]; then
     # release build is always from makefile
     make $DC
