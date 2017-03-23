6ebe39a037c8cb7d1b79126b856f817c32362d29
Marcus Gustafsson <margu478@student.liu.se>
2017-03-23 16:53:54 +0100

    Moved som XML code from sutxml.d and made it more 'modular', atleast the types
    TODO: Remove XML functions from sutenvironment and add support for interfaces

--- plugin/fuzz/source/dextool/plugin/fuzz/xml_parse.d
+++ plugin/fuzz/source/dextool/plugin/fuzz/xml_parse.d
@@ -1,19 +1,41 @@
 module xml_parse;
 
+import std.algorithm;
+import std.array;
+import std.container.array;
+import std.file;
+import std.path;
+import std.string;
+import std.xml;
+
 struct BaseDir {
     string basedir;
     alias basedir this;
 }
 
+struct Global {
+    Array!Primitive prims;
+    Array!SubType subtypes;
+    Array!Record record;
+    Array!Enum enums;
+
+}
 struct Types {
     Array!Primitive prims;
     Array!SubType subtypes;
     Array!Record record;
+    Array!Enum enums;
+    Global glob;
+
 }
+
+
 
 struct Record {
     string name;
-    Variable[string] records;
+    Variable[string] variables;
+
+    this(string name) { this.name = name; }
 }
 
 struct Variable {
@@ -22,7 +44,10 @@ struct Variable {
 }
 
 struct Enum {
+    string name;
     Array!EnumItem enumitems;
+
+    this(string name) { this.name = name; }
 }
 
 struct EnumItem {
@@ -79,6 +104,8 @@ struct Primitive {
     string size;
 
     CPPInterface cppint;
+
+    this(string name, string type, string size) { this.name = name; this.type = type; this.size = size; }
 }
 
 struct CPPInterface {
@@ -96,9 +123,6 @@ private:
         string namespace(string filename) {
         ///TODO: Namespaces should probably only be added if there is any interface in the file
         //dfmt off
-            if (!initDocument(filename) || !checkInterface(filename)) {
-                return "";
-            }
             return dirName(filename)
                 .chompPrefix(this.basedir)
                 .replace(dirSeparator, "::")
@@ -107,22 +131,125 @@ private:
         //dfmt on
         }
 
+        Types getTypes(Element types) {
+            Types xml_types;
+            foreach (Element elem; types.elements) {
+                final switch (elem.tag.name) {
+                    case "SubType":
+                        SubType subtype = SubType(elem.tag.attr["name"], elem.tag.attr["type"]);
+
+                        if (elem.tag.attr.get("min", "-1NOTFOUND") != "-!NOTFOUND") {
+                            subtype.min = elem.tag.attr["min"];
+                        }
+
+                        if (elem.tag.attr.get("max", "-1NOTFOUND") != "-1NOTFOUND") {
+                            subtype.max = elem.tag.attr["max"];
+                        }
+
+                        if (elem.tag.attr.get("unit", "-1NOTFOUND") != "-1NOTFOUND") {
+                            subtype.unit = elem.tag.attr["unit"];
+                        }
+                        xml_types.subtypes.insertBack(subtype);
+                        break;
+
+                    case "Record":
+                        Record rec = Record(elem.tag.attr["name"]);
+                        foreach (Element variable ; elem.elements) {
+                            Variable var = Variable(variable.tag.attr["name"], variable.tag.attr["type"]);
+                            rec.variables[variable.tag.attr["name"]] = var;
+                        }
+                        xml_types.record.insertBack(rec);
+                        break;
+                    case "Enum":
+                        Enum enums = Enum(elem.tag.attr["name"]);
+                        foreach(Element enumitem ; elem.elements) {
+                            enums.enumitems.insertBack(EnumItem(enumitem.tag.attr["name"],
+                                enumitem.tag.attr["value"]));
+                        }
+                        xml_types.enums.insertBack(enums);
+                        break;
+                    case "Primitive":
+                        Primitive prim = Primitive(elem.tag.attr["name"], elem.tag.attr["type"], elem.tag.attr["size"]);
+                        prim.cppint = CPPInterface(elem.elements[0].tag.attr["header"]);
+                        xml_types.prims.insertBack(prim);
+                        break;
+                }
+            }
+            return xml_types;
+        }
+
+        Types[] types(Document doc) {
+            if (doc.tag.name == "Types") { //Everything is a type!
+                return [getTypes(doc)];
+            } else {
+                return doc.elements.filter!(a => a.tag.name == "Types").map!(a => getTypes(a)).array;
+            }
+        }
+
+        Types merge(Types[] old) {
+            Types new_;
+            foreach(Types old_types ; old) {
+                new_.enums = old_types.enums ~ new_.enums;
+                new_.prims = old_types.prims ~ new_.prims;
+                new_.record = old_types.record ~ new_.record;
+                new_.subtypes = old_types.subtypes ~ new_.subtypes;
+            }
+            return new_;
+        }
+
+        Types merge(Types old, Types new_) {
+            new_.enums = old.enums ~ new_.enums;
+            new_.prims = old.prims ~ new_.prims;
+            new_.record = old.record ~ new_.record;
+            new_.subtypes = old.subtypes ~ new_.subtypes;
+
+            return new_;
+        }
+
 public:
     this(BaseDir bdir)
     {
         basedir = bdir;
         xml_files = (dirEntries(this.basedir, "*.xml", SpanMode.breadth).filter!(a => a.isFile)).map!(a => a.name)
             .array;
-        foundNamespaces = xmlinterfaces.map!(a => namespace(a)).filter!(a => a!= "").array;
+        foundNamespaces = xml_files.map!(a => namespace(a)).filter!(a => a!= "").array;
     }
 
     void parseBaseDir() {
+        Types[string] namespaces;
+        Global glob;
         foreach(string xml_file ; xml_files) {
             string curr_ns = namespace(xml_file);
             auto doc = new Document(cast(string) std.file.read(xml_file));
+            Types ntypes = merge(types(doc));
 
+            //This should be merge()
+            if (curr_ns == "global::types") {
+                glob.enums = ntypes.enums ~ glob.enums;
+                glob.prims = ntypes.prims ~ glob.prims;
+                glob.record = ntypes.record ~ glob.record;
+                glob.subtypes = ntypes.subtypes ~ glob.subtypes;
+
+                continue;
+            }
+
+            if (namespaces.get(curr_ns, Types()) != Types()) {
+                namespaces[curr_ns] = merge(ntypes, namespaces[curr_ns]);
+            } else {
+                namespaces[curr_ns] = ntypes;
+            }
+        }
+
+        foreach (string key ; namespaces.byKey) {
+            namespaces[key].glob = glob;
         }
     }
+}
 
 
+int main() {
+    xml_parse xml_p = new xml_parse(BaseDir("sut_unittest/namespaces"));
+    xml_p.parseBaseDir;
+
+    return 0;
 }
