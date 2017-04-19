b201b09ce21ebe092a70c18e9f50c53d70db1ed4
Joakim Brannstrom <joakim.brannstrom@gmx.com>
2017-04-19 07:59:54 +0200

    test: travis script breaks when files start with "-"
    Coverage files created in debug build therefore moving it.

--- travis.sh
+++ travis.sh
@@ -23,6 +23,16 @@ make all -j3
 make test ARGS="--output-on-failure"
 popd
 
+# Copy coverage data so codecov finds it
+# Coverage is only generated when the dmd compiler is used so must ensure that
+# the command always passes. Therefore a "|| true"
+# The coverage files are copied to the project root to allow codecov to find
+# them.
+set +e
+cp -- build/*.lst . || true
+ls
+set -e
+
 # Ensure release build works
 make clean
 mkdir build
@@ -30,11 +40,3 @@ pushd build
 cmake -DLIBCLANG_LIB_PATH=$CLANG_PATH/libclang.so.3.7 ..
 make all -j3
 popd
-
-# Copy coverage data so codecov finds it
-# Coverage is only generated when the dmd compiler is used so must ensure that
-# the command always passes. Therefore a "|| true"
-# The coverage files are copied to the project root to allow codecov to find
-# them.
-set +e
-cp build/*.lst . || true
