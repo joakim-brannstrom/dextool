edc23a1c5480bddedeae0630a585a5beafb0e133
Joakim Brannstrom <joakim.brannstrom@gmx.com>
2017-04-29 23:31:44 +0200

    doc: add a plugin example

---
+++ plugin/example/source/dextool/plugin/cpptestdouble/runner.d
@@ -0,0 +1,150 @@
+/**
+Copyright: Copyright (c) 2017, Joakim Brännström. All rights reserved.
+License: MPL-2
+Author: Joakim Brännström (joakim.brannstrom@gmx.com)
+
+This Source Code Form is subject to the terms of the Mozilla Public License,
+v.2.0. If a copy of the MPL was not distributed with this file, You can obtain
+one at http://mozilla.org/MPL/2.0/.
+
+This file contains an example plugin.
+*/
+
+// for the generic main function to find the plugin the module must have this
+// name.
+module dextool.plugin.runner;
+
+import std.stdio;
+import logger = std.experimental.logger;
+
+import dextool.type : ExitStatusType;
+
+// called by the generic main function in standard.d
+ExitStatusType runPlugin(string[] args) {
+    RawConfiguration pargs;
+    pargs.parse(args);
+
+    // the dextool plugin architecture requires that two lines are printed upon
+    // request by the main function.
+    // a name of the plugin.
+    // a oneliner description.
+    if (pargs.shortPluginHelp) {
+        writeln("example");
+        writeln("plugin description");
+        return ExitStatusType.Ok;
+    } else if (pargs.help) {
+        pargs.printHelp;
+        return ExitStatusType.Ok;
+    } else if (pargs.file.length == 0) {
+        writeln("Missing file --in");
+        return ExitStatusType.Errors;
+    }
+
+    writeln("Run the plugin -d to see the AST");
+
+    import dextool.utility : prependDefaultFlags;
+
+    const auto cflags = prependDefaultFlags(pargs.cflags, "-xc++");
+
+    import std.typecons : Yes;
+    import cpptooling.analyzer.clang.context : ClangContext;
+
+    auto ctx = ClangContext(Yes.useInternalHeaders, Yes.prependParamSyntaxOnly);
+    auto visitor = new TUVisitor;
+
+    import dextool.utility : analyzeFile;
+
+    return analyzeFile(pargs.file, cflags, visitor, ctx);
+}
+
+// It is a good idea to separate the configuration parsing from the main function.
+// It will become complex enough, soon enough.
+
+struct RawConfiguration {
+    import std.getopt : GetoptResult, getopt, defaultGetoptPrinter;
+
+    bool shortPluginHelp;
+    bool help;
+    string file;
+    string[] cflags;
+
+    private GetoptResult help_info;
+
+    void parse(string[] args) {
+        static import std.getopt;
+
+        try {
+            // dfmt off
+            help_info = getopt(args, std.getopt.config.keepEndOfOptions,
+                   "short-plugin-help", "short description of the plugin",  &shortPluginHelp,
+                   "in", "Input file to parse", &file);
+            // dfmt on
+            help = help_info.helpWanted;
+        }
+        catch (std.getopt.GetOptException ex) {
+            logger.error(ex.msg);
+            help = true;
+        }
+
+        import std.algorithm : find;
+        import std.array : array;
+        import std.range : drop;
+
+        // at this point args contain "what is left". What is interesting then is those after "--".
+        cflags = args.find("--").drop(1).array();
+    }
+
+    void printHelp() {
+        import std.stdio : writeln;
+
+        defaultGetoptPrinter("Usage: dextool example [options] [--in=] [-- CFLAGS...]",
+                help_info.options);
+    }
+}
+
+import cpptooling.analyzer.clang.ast : Visitor;
+
+/// A basic visitor showing all the main categories of nodes.
+final class TUVisitor : Visitor {
+    import cpptooling.analyzer.clang.ast;
+    import cpptooling.utility.clang : logNode, mixinNodeLog;
+
+    alias visit = Visitor.visit;
+
+    mixin generateIndentIncrDecr;
+
+    override void visit(const(TranslationUnit) v) {
+        mixin(mixinNodeLog!());
+        v.accept(this);
+    }
+
+    override void visit(const(Attribute) v) {
+        mixin(mixinNodeLog!());
+        v.accept(this);
+    }
+
+    override void visit(const(Declaration) v) {
+        mixin(mixinNodeLog!());
+        v.accept(this);
+    }
+
+    override void visit(const(Directive) v) {
+        mixin(mixinNodeLog!());
+        v.accept(this);
+    }
+
+    override void visit(const(Expression) v) {
+        mixin(mixinNodeLog!());
+        v.accept(this);
+    }
+
+    override void visit(const(Preprocessor) v) {
+        mixin(mixinNodeLog!());
+        v.accept(this);
+    }
+
+    override void visit(const(Reference) v) {
+        mixin(mixinNodeLog!());
+        v.accept(this);
+    }
+}
