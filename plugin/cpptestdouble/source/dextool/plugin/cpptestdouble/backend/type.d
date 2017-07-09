023067cae1c3bf1d084ce4c4e2a3ae9cc2aec83a
Joakim Brannstrom <joakim.brannstrom@gmx.com>
2017-07-09 13:43:32 +0200

    cpptestdouble: split code gen to a separate module

---
+++ plugin/cpptestdouble/source/dextool/plugin/cpptestdouble/backend/type.d
@@ -0,0 +1,95 @@
+/**
+Copyright: Copyright (c) 2017, Joakim Brännström. All rights reserved.
+License: MPL-2
+Author: Joakim Brännström (joakim.brannstrom@gmx.com)
+
+This Source Code Form is subject to the terms of the Mozilla Public License,
+v.2.0. If a copy of the MPL was not distributed with this file, You can obtain
+one at http://mozilla.org/MPL/2.0/.
+*/
+module dextool.plugin.cpptestdouble.backend.type;
+
+import cpptooling.data.representation : CppRoot, CppClass, CppMethod, CppCtor,
+    CppDtor, CFunction, CppNamespace, USRType;
+
+import dsrcgen.cpp : CppModule;
+
+@safe:
+
+enum Kind {
+    none,
+    /// Adapter class
+    adapter,
+    /// gmock class
+    gmock,
+    /// interface for globals
+    testDoubleNamespace,
+    testDoubleSingleton,
+    testDoubleInterface,
+}
+
+struct ImplData {
+    import cpptooling.data.type : CppMethodName;
+    import cpptooling.data.symbol.types : FullyQualifiedNameType;
+
+    CppRoot root;
+
+    /// Tagging of nodes in the root determining how they are handled by the
+    /// code generator step.
+    Kind[size_t] kind;
+    /// Classes found during src analysis.
+    CppClass[FullyQualifiedNameType] classes;
+
+    static auto make() {
+        return ImplData(CppRoot.make);
+    }
+
+    /// Tag an ID with a kind.
+    void tag(size_t id, Kind kind_) {
+        kind[id] = kind_;
+    }
+
+    /// Lookup the tag for an ID.
+    Kind lookup(size_t id) {
+        if (auto k = id in kind) {
+            return *k;
+        }
+
+        return Kind.none;
+    }
+
+    /// Copy an AA of classes.
+    void putForLookup(ref CppClass[FullyQualifiedNameType] other) @trusted {
+        foreach (v; other.byKeyValue) {
+            classes[v.key] = v.value;
+        }
+    }
+
+    /// Store a class that can later be retrieved via its FQN.
+    void putForLookup(CppClass c) {
+        classes[c.fullyQualifiedName] = c;
+    }
+
+    /// Returns: a range containing the class matching fqn, if found.
+    auto lookupClass(FullyQualifiedNameType fqn) @safe {
+        import std.range : only;
+        import std.typecons : NullableRef;
+
+        typeof(only(NullableRef!CppClass())) rval;
+        if (auto c = fqn in classes) {
+            rval = only(NullableRef!CppClass(c));
+        }
+
+        return rval;
+    }
+}
+
+struct GenModules {
+    import dextool.plugin.utility : MakerInitializingClassMembers;
+
+    mixin MakerInitializingClassMembers!GenModules;
+
+    CppModule hdr;
+    CppModule impl;
+    CppModule gmock;
+}
