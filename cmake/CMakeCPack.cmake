a79aa089e827c4cea4b20f9c33f31fe3dbbf3be2
Joakim Brannstrom <joakim.brannstrom@gmx.com>
2017-11-25 17:04:02 +0100

    cmake: add cpack

---
+++ cmake/CMakeCPack.cmake
@@ -0,0 +1,13 @@
+#
+# Common CPack configuration
+#
+set(CPACK_PACKAGE_NAME ${CMAKE_PROJECT_NAME})
+set(CPACK_PACKAGE_VERSION ${DEXTOOL_EMBEDDED_VERSION_PATH})
+set(CPACK_PACKAGE_CONTACT "joakim.brannstrom.public@gmx.com")
+set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "deXtool: C/C++ tooling for test and analysis")
+
+#
+# Debian specifics
+#
+execute_process(COMMAND dpkg --print-architecture OUTPUT_VARIABLE CPACK_DEBIAN_PACKAGE_ARCHITECTURE) 
+set(CPACK_DEBIAN_PACKAGE_SECTION "devel")
