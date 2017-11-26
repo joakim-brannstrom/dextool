9ad00c56f9804f7bfb76f881ec4ec6e7e9f50cca
Joakim Brannstrom <joakim.brannstrom@gmx.com>
2017-11-26 10:52:34 +0100

    travis: travis installation of sqlite3 is too old thus using a local

--- travis.sh
+++ travis.sh
@@ -13,9 +13,14 @@ fi
 
 set -e
 
+# sqlite3
+git clone --depth 1 -b sqlite_src --single-branch https://github.com/joakim-brannstrom/dextool.git sqlite_src
+SQLITE3="-L$ROOT/sqlite_src -lsqlite3"
+
+# debug build
 mkdir build
 pushd build
-cmake -DTEST_WITH_COV=ON -DCMAKE_BUILD_TYPE=Debug -DBUILD_TEST=ON ..
+cmake -DTEST_WITH_COV=ON -DCMAKE_BUILD_TYPE=Debug -DBUILD_TEST=ON -DSQLITE3_LIB="${SQLITE3}" ..
 make all -j3
 make check -j3
 make check_integration -j3
@@ -31,13 +36,14 @@ cp -- build/coverage/*.lst . || true
 ls
 set -e
 
-# Ensure release build works.
+# release build
+
 # Assuming that the tests for release do NOT need to be reran.
 # Testing the install target because it has had problems before
 make clean
 mkdir build
 pushd build
-cmake -DCMAKE_INSTALL_PREFIX=$ROOT/test_install_of_dextool -DCMAKE_BUILD_TYPE=Release ..
+cmake -DCMAKE_INSTALL_PREFIX=$ROOT/test_install_of_dextool -DCMAKE_BUILD_TYPE=Release -DSQLITE3_LIB=${SQLITE3} ..
 make all -j3
 make install
 popd
