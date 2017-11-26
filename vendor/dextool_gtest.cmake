b3d99885c56001c01aa3913772a03693cf0d74fb
Joakim Brannstrom <joakim.brannstrom@gmx.com>
2017-11-26 14:08:58 +0100

    Google test is not maintained in this repo thus move it to
    the vendor subdirectory to make this clear.

---
+++ vendor/dextool_gtest.cmake
@@ -0,0 +1,11 @@
+# vim: filetype=cmake
+
+add_library(gmock_gtest STATIC
+    ${CMAKE_CURRENT_LIST_DIR}/fused_gmock/gmock-gtest-all.cc
+    ${CMAKE_CURRENT_LIST_DIR}/fused_gmock/main.cc
+    )
+set_target_properties(gmock_gtest PROPERTIES
+    COMPILE_FLAGS "-I${CMAKE_CURRENT_LIST_DIR}/fused_gmock"
+    ARCHIVE_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/
+    LIBRARY_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/
+    )
