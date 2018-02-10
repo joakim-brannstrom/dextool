cd9c1739ab870ce85feedc3195abda633cfe6901
Joakim Brannstrom <joakim.brannstrom@gmx.com>
2018-02-10 14:38:19 +0100

    mutate: remove solved TODO

--- plugin/mutate/source/dextool/plugin/mutate/backend/test_mutant.d
+++ plugin/mutate/source/dextool/plugin/mutate/backend/test_mutant.d
@@ -24,8 +24,7 @@ import dextool.plugin.mutate.type : MutationKind;
 
 @safe:
 
-/**
- * TODO add nothrow
+/** Test mutations.
  *
  * Params:
  *  tester = a program to execute that test the mutant. The mutant is marked as alive if the exit code is 0, otherwise it is dead.
