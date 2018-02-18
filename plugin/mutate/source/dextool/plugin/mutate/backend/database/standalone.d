eabf39cc785bd5224543d9b4a0984bf021d8c771
Joakim Brannstrom <joakim.brannstrom@gmx.com>
2018-02-18 12:54:08 +0100

    mutate: create a standalone database interface

--- plugin/mutate/source/dextool/plugin/mutate/backend/database/standalone.d
+++ plugin/mutate/source/dextool/plugin/mutate/backend/database/standalone.d
@@ -1,5 +1,5 @@
 /**
-Copyright: Copyright (c) 2017, Joakim Brännström. All rights reserved.
+Copyright: Copyright (c) 2018, Joakim Brännström. All rights reserved.
 License: MPL-2
 Author: Joakim Brännström (joakim.brannstrom@gmx.com)
 
@@ -7,47 +7,25 @@ This Source Code Form is subject to the terms of the Mozilla Public License,
 v.2.0. If a copy of the MPL was not distributed with this file, You can obtain
 one at http://mozilla.org/MPL/2.0/.
 
-# Sqlite3
-From the sqlite3 manual $(LINK https://www.sqlite.org/datatype3.html):
-Each value stored in an SQLite database (or manipulated by the database
-engine) has one of the following storage classes:
+This module contains the a basic database interface that have minimal dependencies on internal modules.
+It is intended to be reusable from the test suite.
 
-NULL. The value is a NULL value.
-
-INTEGER. The value is a signed integer, stored in 1, 2, 3, 4, 6, or 8 bytes
-depending on the magnitude of the value.
-
-REAL. The value is a floating point value, stored as an 8-byte IEEE floating
-point number.
-
-TEXT. The value is a text string, stored using the database encoding (UTF-8,
-UTF-16BE or UTF-16LE).
-
-BLOB. The value is a blob of data, stored exactly as it was input.
-
-A storage class is more general than a datatype. The INTEGER storage class, for
-example, includes 6 different integer datatypes of different lengths.  This
-makes a difference on disk. But as soon as INTEGER values are read off of disk
-and into memory for processing, they are converted to the most general datatype
-(8-byte signed integer). And so for the most part, "storage class" is
-indistinguishable from "datatype" and the two terms can be used
-interchangeably.
+The only acceptable dependency is ..backend/type.d and database/schema.d
 */
-module dextool.plugin.mutate.backend.database;
+module dextool.plugin.mutate.backend.database.standalone;
 
-import core.time : Duration, dur;
+import core.time : Duration;
 import logger = std.experimental.logger;
 
-import dextool.type : AbsolutePath, Path;
-import dextool.plugin.mutate.backend.type;
-
 import d2sqlite3 : sqlDatabase = Database;
+
+import dextool.type : AbsolutePath, Path;
 
 import dextool.plugin.mutate.backend.database.schema;
 import dextool.plugin.mutate.backend.database.type;
 
-/** Wrapper for a sqlite3 database that provide a uniform, easy-to-use
- * interface for the mutation testing plugin.
+/**
+ * TODO document this.
  */
 struct Database {
     import std.conv : to;
@@ -55,13 +33,11 @@ struct Database {
     import std.typecons : Nullable;
     import dextool.plugin.mutate.backend.type : MutationPoint, Mutation,
         Checksum;
-    import dextool.plugin.mutate.type : MutationOrder;
 
     private sqlDatabase* db;
-    private MutationOrder mut_order;
 
-    static auto make(AbsolutePath db, MutationOrder mut_order) @safe {
-        return Database(initializeDB(db), mut_order);
+    static auto make(AbsolutePath db) @safe {
+        return Database(initializeDB(db));
     }
 
     // Not movable. The database should only be passed around as a reference,
@@ -99,21 +75,6 @@ struct Database {
         typeof(return) rval;
         if (!res.empty) {
             rval = FileId(res.oneValue!long);
-        }
-
-        return rval;
-    }
-
-    Nullable!Checksum getFileChecksum(const Path p) @trusted {
-        import dextool.plugin.mutate.backend.utility : checksum;
-
-        auto stmt = db.prepare("SELECT checksum0,checksum1 FROM files WHERE path=:path");
-        stmt.bind(":path", cast(string) p);
-        auto res = stmt.execute;
-
-        typeof(return) rval;
-        if (!res.empty) {
-            rval = checksum(res.front.peek!long(0), res.front.peek!long(1));
         }
 
         return rval;
@@ -179,71 +140,6 @@ struct Database {
         stmt.execute;
     }
 
-    /** Get the next mutation point + 1 mutant for it that has status unknown.
-     *
-     * TODO to run many instances in parallel the mutation should be locked.
-     * TODO remove nothrow or add a retry-loop
-     *
-     * The chosen point is randomised.
-     *
-     * Params:
-     *  kind = kind of mutation to retrieve.
-     */
-    NextMutationEntry nextMutation(Mutation.Kind[] kinds) nothrow @trusted {
-        import std.algorithm : map;
-        import std.exception : collectException;
-        import std.format : format;
-        import dextool.plugin.mutate.backend.type;
-        import dextool.type : FileName;
-
-        typeof(return) rval;
-
-        auto order = mut_order == MutationOrder.random ? "ORDER BY RANDOM()" : "";
-
-        try {
-            auto prep_str = format("SELECT
-                                   mutation.id,
-                                   mutation.kind,
-                                   mutation.time,
-                                   mutation_point.offset_begin,
-                                   mutation_point.offset_end,
-                                   mutation_point.line,
-                                   mutation_point.column,
-                                   files.path
-                                   FROM mutation,mutation_point,files
-                                   WHERE
-                                   mutation.status == 0 AND
-                                   mutation.mp_id == mutation_point.id AND
-                                   mutation_point.file_id == files.id AND
-                                   mutation.kind IN (%(%s,%)) %s LIMIT 1",
-                    kinds.map!(a => cast(int) a), order);
-            auto stmt = db.prepare(prep_str);
-            // TODO this should work. why doesn't it?
-            //stmt.bind(":kinds", format("%(%s,%)", kinds.map!(a => cast(int) a)));
-            auto res = stmt.execute;
-            if (res.empty) {
-                rval.st = NextMutationEntry.Status.done;
-                return rval;
-            }
-
-            auto v = res.front;
-
-            auto mp = MutationPoint(Offset(v.peek!uint(3), v.peek!uint(4)));
-            mp.mutations = [Mutation(v.peek!long(1).to!(Mutation.Kind))];
-            auto pkey = MutationId(v.peek!long(0));
-            auto file = Path(FileName(v.peek!string(7)));
-            auto sloc = SourceLoc(v.peek!uint(5), v.peek!uint(6));
-
-            rval.entry = MutationEntry(pkey, file, sloc, mp, v.peek!long(2).dur!"msecs");
-        }
-        catch (Exception e) {
-            rval.st = NextMutationEntry.Status.queryError;
-            collectException(logger.warning(e.msg));
-        }
-
-        return rval;
-    }
-
     Nullable!MutationEntry getMutation(MutationId id) nothrow @trusted {
         import dextool.plugin.mutate.backend.type;
         import dextool.type : FileName;
@@ -278,6 +174,8 @@ struct Database {
             auto file = Path(FileName(v.peek!string(7)));
             auto sloc = SourceLoc(v.peek!uint(5), v.peek!uint(6));
 
+            import core.time : dur;
+
             rval = MutationEntry(pkey, file, sloc, mp, v.peek!long(2).dur!"msecs");
         }
         catch (Exception e) {
@@ -285,51 +183,6 @@ struct Database {
         }
 
         return rval;
-    }
-
-    void iterateMutants(const Mutation.Kind[] kinds, void delegate(const ref IterateMutantRow) dg) nothrow @trusted {
-        import std.algorithm : map;
-        import std.format : format;
-        import dextool.plugin.mutate.backend.utility : checksum;
-
-        immutable all_mutants = "SELECT
-            mutation.id,
-            mutation.status,
-            mutation.kind,
-            mutation.time,
-            mutation_point.offset_begin,
-            mutation_point.offset_end,
-            mutation_point.line,
-            mutation_point.column,
-            files.path,
-            files.checksum0,
-            files.checksum1
-            FROM mutation,mutation_point,files
-            WHERE
-            mutation.kind IN (%(%s,%)) AND
-            mutation.mp_id == mutation_point.id AND
-            mutation_point.file_id == files.id
-            ORDER BY mutation.status";
-
-        try {
-            auto res = db.prepare(format(all_mutants, kinds.map!(a => cast(int) a))).execute;
-            foreach (ref r; res) {
-                IterateMutantRow d;
-                d.id = MutationId(r.peek!long(0));
-                d.mutation = Mutation(r.peek!int(2).to!(Mutation.Kind),
-                        r.peek!int(1).to!(Mutation.Status));
-                auto offset = Offset(r.peek!uint(4), r.peek!uint(5));
-                d.mutationPoint = MutationPoint(offset, null);
-                d.file = r.peek!string(8);
-                d.fileChecksum = checksum(r.peek!long(9), r.peek!long(10));
-                d.sloc = SourceLoc(r.peek!uint(6), r.peek!uint(7));
-
-                dg(d);
-            }
-        }
-        catch (Exception e) {
-            logger.error(e.msg).collectException;
-        }
     }
 
     /** Reset all mutations of kinds with the status `st` to unknown.
@@ -465,13 +318,4 @@ struct Database {
             }
         }
     }
-}
-
-struct IterateMutantRow {
-    MutationId id;
-    Mutation mutation;
-    MutationPoint mutationPoint;
-    Path file;
-    Checksum fileChecksum;
-    SourceLoc sloc;
 }
