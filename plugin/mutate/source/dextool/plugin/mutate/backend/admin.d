04df6c9506ae438a631d2c9a47cecd8b20b12313
Joakim Brannstrom <joakim.brannstrom@gmx.com>
2018-03-22 00:16:01 +0100

    mutate: add admin interface for removing mutations
    It is a bit too over-zealous for now because it removes the mutation
    points. It should be improved in the future.

--- plugin/mutate/source/dextool/plugin/mutate/backend/admin.d
+++ plugin/mutate/source/dextool/plugin/mutate/backend/admin.d
@@ -16,21 +16,46 @@ import logger = std.experimental.logger;
 
 import dextool.type;
 
-import dextool.plugin.mutate.type : MutationKind;
+import dextool.plugin.mutate.type : MutationKind, AdminOperation;
 import dextool.plugin.mutate.backend.database : Database;
 import dextool.plugin.mutate.backend.type : Mutation;
 
-ExitStatusType runAdmin(ref Database db, MutationKind[] mutations,
-        Mutation.Status status, Mutation.Status to_status) @safe nothrow {
+ExitStatusType runAdmin(ref Database db, AdminOperation admin_op,
+        MutationKind[] mutations, Mutation.Status status, Mutation.Status to_status) @safe nothrow {
     import dextool.plugin.mutate.backend.utility;
 
     const auto kinds = dextool.plugin.mutate.backend.utility.toInternal(mutations);
 
+    final switch (admin_op) {
+    case AdminOperation.reset:
+        return resetMutant(db, kinds, status, to_status);
+    case AdminOperation.remove:
+        return removeMutant(db, kinds);
+    }
+}
+
+private:
+
+ExitStatusType resetMutant(ref Database db, const Mutation.Kind[] kinds,
+        Mutation.Status status, Mutation.Status to_status) @safe nothrow {
     try {
         db.resetMutant(kinds, status, to_status);
     }
     catch (Exception e) {
         logger.error(e.msg).collectException;
+        return ExitStatusType.Errors;
+    }
+
+    return ExitStatusType.Ok;
+}
+
+ExitStatusType removeMutant(ref Database db, const Mutation.Kind[] kinds) @safe nothrow {
+    try {
+        db.removeMutant(kinds);
+    }
+    catch (Exception e) {
+        logger.error(e.msg).collectException;
+        return ExitStatusType.Errors;
     }
 
     return ExitStatusType.Ok;
