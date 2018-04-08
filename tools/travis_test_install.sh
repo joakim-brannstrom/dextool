d8073e878c5877e15b2a85167c4c2996992919a5
Joakim Brannstrom <joakim.brannstrom@gmx.com>
2018-04-08 11:51:02 +0200

    travis: add file installation test

--- tools/travis_test_install.sh
+++ tools/travis_test_install.sh
@@ -4,3 +4,13 @@ set -exo pipefail
 
 # The installation shall be to test_install_of_dextool/ containing binaries
 ls test_install_of_dextool
+
+# Test that it is exactly the expected number of binaries and libraries that are installed.
+# The intent is to find a mismatch on the CI and thus force the developer to check it out
+COUNT_INSTALLED=$(find test_install_of_dextool |wc -l)
+EXPECTED_INSTALLED=15
+if [[ "${COUNT_INSTALLED}" -ne $EXPECTED_INSTALLED ]]; then
+    find test_install_of_dextool
+    echo "Number of installed files and directories do not match the expected: $EXPECTED_INSTALLED"
+    exit 1
+fi
