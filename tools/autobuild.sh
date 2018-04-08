83ba58aea1f44b70495967a868245353de04923b
Joakim Brannstrom <joakim.brannstrom@gmx.com>
2018-04-08 10:29:32 +0200

    tools: move autobuild to tools directory

--- tools/autobuild.sh
+++ tools/autobuild.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+# run the script from the root of the deXtool repo
+AUTOBUILD_TOOL=tools/autobuild
+AUTOBUILD_BIN=autobuild.bin
+
 set -e
-pushd .autobuild
+pushd $AUTOBUILD_TOOL
 dub build --skip-registry=all -b release
 popd
+
 ./.autobuild.bin "$@"
