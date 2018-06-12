16adcb0118f8fc22decb522b62673f47c080943d
Joakim Brannstrom <joakim.brannstrom@gmx.com>
2018-06-12 14:09:00 +0200

    Remove unused code

--- source/dextool/logger_conf.d
+++ source/dextool/logger_conf.d
@@ -10,75 +10,44 @@ one at http://mozilla.org/MPL/2.0/.
 module dextool.logger_conf;
 
 import std.algorithm : among;
-import std.stdio : writeln, writefln, stderr, stdout;
+import std.stdio : writefln, stderr, stdout;
 
 import logger = std.experimental.logger;
 
 class SimpleLogger : logger.Logger {
-    import std.conv;
-
-    int line = -1;
-    string file = null;
-    string func = null;
-    string prettyFunc = null;
-    string msg = null;
-    logger.LogLevel lvl;
-
     this(const logger.LogLevel lv = logger.LogLevel.info) {
         super(lv);
     }
 
     override void writeLogMsg(ref LogEntry payload) @trusted {
-        this.line = payload.line;
-        this.file = payload.file;
-        this.func = payload.funcName;
-        this.prettyFunc = payload.prettyFuncName;
-        this.lvl = payload.logLevel;
-        this.msg = payload.msg;
-
         auto out_ = stderr;
 
         if (payload.logLevel.among(logger.LogLevel.info, logger.LogLevel.trace)) {
             out_ = stdout;
         }
 
-        out_.writefln("%s: %s", text(this.lvl), this.msg);
+        out_.writefln("%s: %s", payload.logLevel, payload.msg);
     }
 }
 
 class DebugLogger : logger.Logger {
-    import std.conv;
-
-    int line = -1;
-    string file = null;
-    string func = null;
-    string prettyFunc = null;
-    string msg = null;
-    logger.LogLevel lvl;
-
     this(const logger.LogLevel lv = logger.LogLevel.trace) {
         super(lv);
     }
 
     override void writeLogMsg(ref LogEntry payload) @trusted {
-        this.line = payload.line;
-        this.file = payload.file;
-        this.func = payload.funcName;
-        this.prettyFunc = payload.prettyFuncName;
-        this.lvl = payload.logLevel;
-        this.msg = payload.msg;
-
         auto out_ = stderr;
 
         if (payload.logLevel.among(logger.LogLevel.info, logger.LogLevel.trace)) {
             out_ = stdout;
         }
 
-        if (this.line == -1) {
-            out_.writefln("%s: %s", text(this.lvl), this.msg);
+        if (payload.line == -1) {
+            out_.writefln("%s: %s", payload.logLevel, payload.msg);
         } else {
             // Example of standard: 2016-05-01T22:31:54.019:type.d:retrieveType:159 c:@S@Foo:struct Foo
-            out_.writefln("%s: %s [%s:%d]", text(this.lvl), this.msg, this.func, this.line);
+            out_.writefln("%s: %s [%s:%d]", payload.logLevel, payload.msg,
+                    payload.funcName, payload.line);
         }
     }
 }
