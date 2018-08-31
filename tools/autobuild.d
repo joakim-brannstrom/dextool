f5f45649150187f9adb57884bf52bfca1068e12b
Joakim Brannstrom <joakim.brannstrom@gmx.com>
2018-08-31 22:38:52 +0200

    test: migrate shell script to a D program

---
+++ tools/autobuild.d
@@ -0,0 +1,22 @@
+#!/usr/bin/env rdmd
+
+import std.process;
+
+int main(string[] args) {
+    // run the script from the root of the deXtool repo
+    const AUTOBUILD_TOOL = "tools/autobuild";
+    const AUTOBUILD_BIN = "autobuild.bin";
+
+    if (spawnProcess(["dub", "build", "--skip-registry=all", "-b", "release"],
+            null, Config.none, AUTOBUILD_TOOL).wait != 0) {
+        return 1;
+    }
+
+    args = () {
+        if (args.length > 1)
+            return args[1 .. $];
+        return null;
+    }();
+
+    return spawnProcess(["./.autobuild.bin"] ~ args).wait;
+}
