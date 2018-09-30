88b8de5cf7a9baebfa2c59e5156c78aeb65245e5
Joakim Brannstrom <joakim.brannstrom@gmx.com>
2018-09-30 12:14:19 +0200

    mutate: reset any modified files to the original state in the examples

--- plugin/mutate/examples/gtest/2_analyze.sh
+++ plugin/mutate/examples/gtest/2_analyze.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 cd googletest
+# reset the repos files in case the mutation testing where interrupted and left
+# a mutated file behind
+git checkout .
 
 # Generate a database of all mutation points:
 dextool mutate analyze
--- plugin/mutate/examples/gtest/4_report.sh
+++ plugin/mutate/examples/gtest/4_report.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 cd googletest
+# reset the repos files in case the mutation testing where interrupted and left
+# a mutated file behind
+git checkout .
 
 dextool mutate report --section tc_stat --section summary --section killed --section tc_killed_no_mutants --mutant lcr
