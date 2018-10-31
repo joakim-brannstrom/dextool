b95c09100474551392e2106853869a5b2e16bff8
nils-petersson <nils.h.petersson@gmail.com>
2018-10-31 11:34:54 +0100

    eqdetect: no longer generates namespaces for klee-files + handle functions without parameters

--- plugin/eqdetect/source/dextool/plugin/eqdetect/backend/codegenerator.d
+++ plugin/eqdetect/source/dextool/plugin/eqdetect/backend/codegenerator.d
@@ -16,35 +16,32 @@ module dextool.plugin.eqdetect.backend.codegenerator;
 
 import dextool.plugin.eqdetect.backend.type : Mutation;
 import dextool.type : FileName;
+import std.typecons : Tuple;
 
 @safe:
 
 class SnippetFinder {
     import clang.Cursor;
 
-    static string[] generate(Cursor cursor, Mutation mutation) {    
+    static string[] generate(Cursor cursor, Mutation mutation) {
         import std.stdio : File;
         import std.file : getSize;
 
         auto file = File(cursor.extent.path, "r");
         auto buffer = file.rawRead(new char[getSize(cursor.extent.path)]);
-
-        buffer = buffer[cursor.extent.start.offset .. cursor.extent.end.offset];
-        mutation.offset_begin = mutation.offset_begin - cursor.extent.start.offset;
-        mutation.offset_end = mutation.offset_end - cursor.extent.start.offset;
         auto mutation_buffer = generateMut(buffer, mutation);
 
         import std.utf : validate, toUTF8;
 
         validate(buffer);
         validate(mutation_buffer);
-        return [generateNamespace(toUTF8(buffer), "source"),
-            generateNamespace(toUTF8(mutation_buffer), "mutant")];
+        return [toUTF8(buffer), toUTF8(mutation_buffer)];
     }
 
     static auto generateMut(char[] content, Mutation mutation) {
         import dextool.plugin.mutate.backend.generate_mutant : makeMutation;
-        import dextool.plugin.mutate.backend.type : Offset, mutationStruct = Mutation;
+        import dextool.plugin.mutate.backend.type : Offset,
+            mutationStruct = Mutation;
 
         auto mut = makeMutation(cast(mutationStruct.Kind) mutation.kind, mutation.lang);
         auto temp = mut.top() ~ content[0 .. mutation.offset_begin];
@@ -81,7 +78,9 @@ class SnippetFinder {
             }
 
             // remove last comma that was inserted into the parameters
-            func_params = func_params[0 .. $ - 1];
+            if (func_params.length != 0) {
+                func_params = func_params[0 .. $ - 1];
+            }
 
             // symbolic variable for each parameter
             for (int i = 0; i < params.length; i++) {
@@ -89,8 +88,8 @@ class SnippetFinder {
             }
 
             // equivalence detection
-            with (if_(format(`source::%s(%s) == mutant::%s(%s)`, function_name,
-                    func_params, function_name, func_params))) {
+            with (if_(format(`%s(%s) == m_%s(%s)`, function_name, func_params,
+                    function_name, func_params))) {
                 return_(`1`);
             }
 
@@ -101,16 +100,5 @@ class SnippetFinder {
 
         }
         return code.render;
-    }
-
-    static string generateNamespace(string namespace_code, string namespace_name) {
-        namespace_code = `namespace ` ~ namespace_name ~ ` {
-
-` ~ namespace_code ~ `
-
-}
-`;
-
-        return namespace_code;
     }
 }
