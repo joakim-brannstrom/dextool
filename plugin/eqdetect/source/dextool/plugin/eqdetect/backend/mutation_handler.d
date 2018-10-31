df313aac5fb491fa8d75287d7af49031d8d21339
nils-petersson <nils.h.petersson@gmail.com>
2018-10-31 11:38:24 +0100

    eqdetect: no longer uses docker for KLEE and changing names in mutation-file instead of using namespaces

--- plugin/eqdetect/source/dextool/plugin/eqdetect/backend/mutation_handler.d
+++ plugin/eqdetect/source/dextool/plugin/eqdetect/backend/mutation_handler.d
@@ -19,8 +19,9 @@ import dextool.plugin.eqdetect.backend.type : Mutation;
 import logger = std.experimental.logger;
 import dextool.plugin.eqdetect.backend.type : ErrorResult;
 
-void handleMutation(TUVisitor visitor, Mutation mutation){
+void handleMutation(TUVisitor visitor, Mutation mutation) {
     // create file for sourcecode, the mutant and code prepared for KLEE
+    mutationEnhancer(visitor, mutation);
     createSourceFiles(visitor, mutation);
     // start the symbolic execution in KLEE
     runKLEE();
@@ -30,7 +31,46 @@ void handleMutation(TUVisitor visitor, Mutation mutation){
     markMutation(errorResult, mutation);
 }
 
-void createSourceFiles(TUVisitor visitor, Mutation mutation){
+void mutationEnhancer(TUVisitor visitor, Mutation mutation) {
+    import std.stdio;
+
+    import std.algorithm;
+
+    visitor.offsets.sort!("a[0] > b[0]");
+    string text = visitor.generatedSource.render;
+    for (int i = 0; i < visitor.offsets.length; i++) {
+        text = nameReplacer(text, visitor.offsets[i]);
+        if (mutation.offset_begin > visitor.offsets[i][0]
+                && mutation.offset_end > visitor.offsets[i][1]) {
+            mutation.offset_begin += 2;
+            mutation.offset_end += 2;
+        } else if (mutation.offset_begin <= visitor.offsets[i][0]
+                && mutation.offset_end >= visitor.offsets[i][1]) {
+            mutation.offset_end += 2;
+        }
+    }
+
+    import dextool.plugin.mutate.backend.generate_mutant : makeMutation;
+    import dextool.plugin.mutate.backend.type : Offset,
+        mutationStruct = Mutation;
+
+    auto mut = makeMutation(cast(mutationStruct.Kind) mutation.kind, mutation.lang);
+    auto temp = mut.top() ~ text[0 .. mutation.offset_begin];
+    temp = temp ~ mut.mutate(text[mutation.offset_begin .. mutation.offset_end]);
+    temp = temp ~ text[mutation.offset_end .. $];
+    writeln(temp);
+    visitor.generatedMutation.text(temp);
+}
+
+import dextool.plugin.mutate.backend.type : Offset;
+import std.typecons : Tuple;
+
+string nameReplacer(string text, Tuple!(uint, uint) offset) {
+    text = text[0 .. offset[0]] ~ "m_" ~ text[offset[0] .. $];
+    return text;
+}
+
+void createSourceFiles(TUVisitor visitor, Mutation mutation) {
     import std.path : baseName;
     import dextool.plugin.eqdetect.backend : writeToFile, SnippetFinder;
     import dextool.type : FileName;
@@ -39,41 +79,40 @@ void createSourceFiles(TUVisitor visitor, Mutation mutation){
             baseName(mutation.path), mutation.kind, mutation.id, "_source_");
     FileName mutant_path = writeToFile(visitor.generatedMutation.render,
             baseName(mutation.path), mutation.kind, mutation.id, "_mutant_");
-    auto s = SnippetFinder.generateKlee(visitor.function_params,
-            source_path, mutant_path, visitor.function_name);
+    auto s = SnippetFinder.generateKlee(visitor.function_params, source_path,
+            mutant_path, visitor.function_name);
     writeToFile(s, baseName(mutation.path), mutation.kind, mutation.id, "_klee_");
 }
 
-void runKLEE(){
+void runKLEE() {
     import std.process : executeShell;
     import std.file : getcwd;
     import std.format : format;
 
-    // Spawn a shell and create the klee-container with a mounted volume (current build directory)
-    // The created container will execute the klee.sh script and after execution get removed
     logger.info("KLEE execution started");
-    auto klee_exec_out = executeShell(format(
-            "docker run --rm -it --name=klee_container4 -v %s:/home/klee/mounted klee/klee mounted/klee.sh",
-            getcwd())).output;
-    logger.info(klee_exec_out);
+
+    //TODO: get rid of klee.sh
+    logger.info(executeShell("./klee.sh").output);
 
     // cleanup the temporary directory created
     executeShell("rm -rf eqdetect_generated_files/*");
 }
 
-ErrorResult parseKLEE(){
+ErrorResult parseKLEE() {
     // parse the result from KLEE execution
     import dextool.plugin.eqdetect.backend.parser : errorTextParser;
+
     ErrorResult errorResult = errorTextParser("result.txt");
 
     // remove the result-file
     import std.file : remove;
+
     remove("result.txt");
 
     return errorResult;
 }
 
-void markMutation(ErrorResult errorResult, Mutation mutation){
+void markMutation(ErrorResult errorResult, Mutation mutation) {
     import dextool.plugin.mutate.backend.type : mutationStruct = Mutation;
     import dextool.plugin.eqdetect.backend.dbhandler : setEquivalence;
 
@@ -83,6 +122,12 @@ void markMutation(ErrorResult errorResult, Mutation mutation){
         break;
     case "Halt":
         setEquivalence(mutation.id, mutationStruct.eq.timeout);
+        break;
+    case "ERROR":
+        setEquivalence(mutation.id, mutationStruct.eq.unknown);
+        break;
+    case "Assert":
+        setEquivalence(mutation.id, mutationStruct.eq.not_equivalent);
         break;
     default:
         setEquivalence(mutation.id, mutationStruct.eq.not_equivalent);
