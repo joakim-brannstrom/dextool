a55614d19b95259ce65951753f41ecc81646a6c1
nils-petersson <nils.h.petersson@gmail.com>
2018-11-15 11:33:10 +0100

    eqdetect: added support for headerfiles and non-primitive data types + some refactoring.

--- plugin/eqdetect/source/dextool/plugin/eqdetect/backend/visitor.d
+++ plugin/eqdetect/source/dextool/plugin/eqdetect/backend/visitor.d
@@ -34,13 +34,19 @@ final class TUVisitor : Visitor {
     mixin generateIndentIncrDecr;
     CModule generatedSource;
     CModule generatedMutation;
+    CModule generatedSourceHeader;
+    CModule generatedMutationHeader;
+
     bool generated = false;
     string function_name;
     string[] function_params;
+    bool isFunctionVoid;
+    Cursor[] semanticParentList;
+    string[] types;
 
-    import std.typecons : Tuple;
-
+    Offset includeOffset;
     Offset[] offsets;
+    Offset[] headerOffsets;
 
     import dextool.plugin.eqdetect.backend : Mutation;
 
@@ -49,10 +55,31 @@ final class TUVisitor : Visitor {
     this(Mutation m) {
         this.generatedSource = new CModule();
         this.generatedMutation = new CModule();
+        this.generatedSourceHeader = new CModule();
+        this.generatedMutationHeader = new CModule();
         this.mutation = m;
+        this.includeOffset.begin = -1;
+        this.includeOffset.end = -1;
     }
 
     override void visit(const(TranslationUnit) v) {
+        mixin(mixinNodeLog!());
+        import std.path : extension;
+        import dextool.plugin.eqdetect.backend : SnippetFinder;
+        if(extension(v.cursor.extent.path) == ".cpp"){
+            auto s = SnippetFinder.generateSource(v.cursor, this.mutation);
+            generatedSource.text(s);
+        }
+        else if(extension(v.cursor.extent.path) == ".hpp"){
+            auto s = SnippetFinder.generateSource(v.cursor, this.mutation);
+            generatedSourceHeader.text(s);
+        }
+
+        v.accept(this);
+    }
+
+
+    override void visit(const(TypedefDecl) v) {
         mixin(mixinNodeLog!());
         v.accept(this);
     }
@@ -64,44 +91,74 @@ final class TUVisitor : Visitor {
 
     override void visit(const(DeclRefExpr) v) {
         mixin(mixinNodeLog!());
-        saveOffsets(v.cursor);
+
+        checkTypes(v.cursor);
+        v.accept(this);
+    }
+
+    override void visit(const(CallExpr) v) {
+        mixin(mixinNodeLog!());
+        checkTypes(v.cursor);
         v.accept(this);
     }
 
     override void visit(const(ParmDecl) v) {
         mixin(mixinNodeLog!());
-        saveOffsets(v.cursor);
+        checkTypes(v.cursor);
         v.accept(this);
     }
 
     override void visit(const(VarDecl) v) {
         mixin(mixinNodeLog!());
-        saveOffsets(v.cursor);
+        checkTypes(v.cursor);
         v.accept(this);
     }
 
+
     override void visit(const(CxxMethod) v) {
         mixin(mixinNodeLog!());
-        saveOffsets(v.cursor);
+        checkTypes(v.cursor);
+        saveMutationInformation(v.cursor);
         v.accept(this);
     }
 
     override void visit(const(FunctionDecl) v) {
         mixin(mixinNodeLog!());
-        saveOffsets(v.cursor);
-        generateCode(v.cursor); //TODO: call on this elsewhere
+        checkTypes(v.cursor);
+        saveMutationInformation(v.cursor);
+        v.accept(this);
+    }
+
+    // To avoid namechange of for example 'public:'
+    override void visit(const(CxxAccessSpecifier) v) {
+        mixin(mixinNodeLog!());
+        v.accept(this);
+    }
+
+    override void visit(const(ClassDecl) v) {
+        mixin(mixinNodeLog!());
+        saveTypes(v.cursor);
+        checkTypes(v.cursor);
+        v.accept(this);
+    }
+
+    override void visit(const(StructDecl) v) {
+        mixin(mixinNodeLog!());
+        saveTypes(v.cursor);
+        checkTypes(v.cursor);
         v.accept(this);
     }
 
     override void visit(const(Declaration) v) {
         mixin(mixinNodeLog!());
-        saveOffsets(v.cursor);
+        checkTypes(v.cursor);
         v.accept(this);
     }
 
+
     override void visit(const(MemberRefExpr) v) {
         mixin(mixinNodeLog!());
-        saveOffsets(v.cursor);
+        checkTypes(v.cursor);
         v.accept(this);
     }
 
@@ -115,8 +172,20 @@ final class TUVisitor : Visitor {
         v.accept(this);
     }
 
+    override void visit(const(InclusionDirective) v) {
+        mixin(mixinNodeLog!());
+        if(isSameBaseName(mutation.path, v.cursor.spelling)){
+            saveOffsets(v.cursor);
+        }
+        v.accept(this);
+    }
+
     override void visit(const(Preprocessor) v) {
         mixin(mixinNodeLog!());
+        import std.path : stripExtension;
+        if(stripExtension(v.cursor.extent.path) == stripExtension(mutation.path)){
+            checkTypes(v.cursor);
+        }
         v.accept(this);
     }
 
@@ -130,47 +199,105 @@ final class TUVisitor : Visitor {
         v.accept(this);
     }
 
+
+
     override void visit(const(Namespace) v) {
         mixin(mixinNodeLog!());
         v.accept(this);
     }
 
-    //TODO: Fix ugly implementation, doesn't work in all cases (ex. int i = 0)
-    void saveOffsets(Cursor c) {
-        import std.path : buildNormalizedPath;
-        import std.file : getcwd;
+    bool isSameBaseName(string a, string b){
+        import std.path: stripExtension, baseName;
+        return stripExtension(baseName(a)) == stripExtension(baseName(b));
+    }
 
-        if (buildNormalizedPath(getcwd(), mutation.path) == c.definition.location.path
-                && buildNormalizedPath(getcwd(), mutation.path) == c.extent.path) {
-            Offset offset;
-            import std.stdio : File, writeln;
-            import std.file : getSize;
-            import std.string : indexOf;
+    bool mutationInInterval(Cursor c) {
+        return ((c.extent.end.offset >= mutation.offset.end) && (c.extent.start.offset <= mutation.offset.begin));
+    }
 
-            auto file = File(c.extent.path, "r");
-            auto buffer = file.rawRead(new char[getSize(c.extent.path)]);
-            buffer = buffer[c.extent.start.offset .. c.extent.end.offset];
-            int name_offset = cast(int) indexOf(buffer, c.spelling);
-            offset.begin = c.extent.start.offset + name_offset;
-            offset.end = c.extent.end.offset - (
-                    c.extent.end.offset - c.extent.start.offset - cast(
-                    uint) c.spelling.length - name_offset);
-
-            offsets ~= offset;
+    void saveTypes(Cursor c){
+        import std.path : buildNormalizedPath, stripExtension;
+        if(stripExtension(c.extent.path) == stripExtension(mutation.path)){
+            types ~= c.spelling;
         }
     }
 
-    void generateCode(Cursor c) {
-        import std.path : baseName;
+    @trusted void checkTypes(Cursor c){
+        foreach(t;c.tokens){
+            import std.algorithm: canFind;
 
-        if (!generated && c.extent.path.length != 0
-                && baseName(mutation.path) == baseName(c.extent.path)) {
-            import dextool.plugin.eqdetect.backend : SnippetFinder;
+            if (types.canFind(t.spelling)) {
+                saveOffsets(c, t.spelling);
+            }
+        }
+        saveOffsets(c);
+    }
 
-            auto s = SnippetFinder.generateSource(c, this.mutation);
+    @trusted Offset findOffset(Cursor c, string name){
+        Offset offset;
+        import std.stdio : File;
+        import std.file : getSize;
+        import std.string : indexOf;
 
-            this.generatedSource.text(s);
+        offset.begin = -1;
+        offset.end = -1;
 
+        foreach (token; c.tokens){
+            // Handles both types and includes of files
+            if (token.spelling == name || token.spelling == ("\"" ~ name ~ "\"") ){
+                offset.begin = token.extent.start.offset;
+                offset.end = token.extent.end.offset;
+
+                return offset;
+            }
+        }
+        return offset;
+    }
+
+    void saveOffsets(Cursor c, string name = "") {
+        import std.path: extension;
+
+        if (name == ""){name = c.spelling;}
+
+        if (isSameBaseName(c.extent.path, mutation.path)){
+            Offset offset = findOffset(c, name);
+
+            if(offset.begin != -1 && offset.end != -1){
+                import std.algorithm: canFind;
+                import std.path : extension;
+                import clang.c.Index;
+
+                if(c.kind == CXCursorKind.inclusionDirective){
+                    includeOffset = offset;
+                }
+                else if(extension(c.extent.path) == ".cpp"){
+                    if (!offsets.canFind(offset)) {
+                        offsets ~= offset;
+                    }
+                }
+                else if(extension(c.extent.path) == ".hpp"){
+                    if (!headerOffsets.canFind(offset)) {
+                        headerOffsets ~= offset;
+                    }
+                }
+            }
+        }
+    }
+
+    void findSemanticParents(Cursor c){
+        import std.path: isAbsolute;
+        if(!isAbsolute(c.semanticParent.spelling)){
+            semanticParentList ~= c.semanticParent;
+
+            findSemanticParents(c.semanticParent);
+        }
+    }
+
+    void saveMutationInformation(Cursor c) {
+        if (!generated && c.extent.path.length != 0 && mutationInInterval(c)
+                && isSameBaseName(mutation.path, c.extent.path)) {
+
+            findSemanticParents(c);
             getFunctionDecl(c);
 
             generated = true;
@@ -180,8 +307,9 @@ final class TUVisitor : Visitor {
     @trusted void getFunctionDecl(Cursor c) {
         import clang.c.Index;
 
-        function_name = c.tokens[1].spelling;
-
+        function_name = c.spelling;
+        import std.stdio;
+        isFunctionVoid = (c.FunctionCursor.resultType.spelling == "void");
         foreach (child; c.children) {
             if (child.kind == CXCursorKind.parmDecl) {
                 string tmp = "";
