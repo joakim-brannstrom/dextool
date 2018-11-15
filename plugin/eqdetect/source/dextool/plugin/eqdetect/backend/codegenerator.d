6dd30b1e5d8a527c80823f52bbc6b3bf534bad32
nils-petersson <nils.h.petersson@gmail.com>
2018-11-15 11:07:15 +0100

    eqdetect: supporting void-functions and functions within namespaces/classes/structs.

--- plugin/eqdetect/source/dextool/plugin/eqdetect/backend/codegenerator.d
+++ plugin/eqdetect/source/dextool/plugin/eqdetect/backend/codegenerator.d
@@ -17,11 +17,14 @@ module dextool.plugin.eqdetect.backend.codegenerator;
 import dextool.plugin.eqdetect.backend.type : Mutation;
 import dextool.type : FileName;
 import std.typecons : Tuple;
+import dsrcgen.c : CModule;
+import clang.c.Index : CXCursorKind;
 
 @safe:
 
 class SnippetFinder {
     import clang.Cursor;
+    import std.format : format;
 
     static string generateSource(Cursor cursor, Mutation mutation) {
         import std.stdio : File;
@@ -51,24 +54,47 @@ class SnippetFinder {
 
     // Generating code that will be used by klee to evaluate the source and mutant code.
     static auto generateKlee(string[] params, FileName source_name,
-            FileName mutant_name, string function_name) {
-        import std.format : format;
-        import dsrcgen.c : CModule;
+            FileName mutant_name, string function_name, Cursor[] semanticParentList, bool isFunctionVoid) {
 
         auto code = new CModule();
+        import std.stdio;
+        includeGenerator(code, source_name, mutant_name);
+        // add klee-main
+        mainGenerator(code, params, function_name, semanticParentList, isFunctionVoid);
+        return code.render;
+    }
 
+    static void includeGenerator(ref CModule code, FileName source_name, FileName mutant_name){
         // add klee imports
         code.include(`<klee/klee.h>`);
         code.include(`<assert.h>`);
 
         // add import for the files that are being tested
-        code.include(format(`%s`, source_name));
-        code.include(format(`%s`, mutant_name));
+        code.include(source_name);
+        code.include(mutant_name);
+    }
 
-        string func_params;
+    static void nameSpaceStringGenerator(Cursor[] semanticParentList,
+        ref string namespaceString, ref string mutNamespaceString){
 
-        // add klee-main
-        with (code.func_body(`int`, `main`)) {
+        import std.conv : to;
+
+        for (int i = to!int(semanticParentList.length)-1; i >= 1; i--){
+            if (semanticParentList[i].kind == CXCursorKind.namespace){
+                namespaceString ~= semanticParentList[i].spelling ~ "::";
+                mutNamespaceString ~= semanticParentList[i].spelling ~ "::";
+            } else {
+                namespaceString ~= semanticParentList[i].spelling ~ "::";
+                mutNamespaceString ~= "m_" ~ semanticParentList[i].spelling ~ "::";
+            }
+        }
+    }
+
+    static void mainGenerator(ref CModule code, string[] params,
+     string function_name, Cursor[] semanticParentList, bool isFunctionVoid){
+
+         string func_params;
+         with (code.func_body(`int`, `main`)) {
 
             // variable declaration
             for (int i = 0; i < params.length; i++) {
@@ -87,17 +113,66 @@ class SnippetFinder {
             }
 
             // equivalence detection
-            with (if_(format(`%s(%s) == m_%s(%s)`, function_name, func_params,
-                    function_name, func_params))) {
-                return_(`1`);
+            string namespaceString, mutNamespaceString, lastObject, lastMutObject;
+
+            nameSpaceStringGenerator(semanticParentList, namespaceString, mutNamespaceString);
+            import std.stdio;
+            if(semanticParentList.length != 0){
+                if (semanticParentList[0].kind == CXCursorKind.namespace){
+                    namespaceString ~= semanticParentList[0].spelling ~ "::";
+                } else if (semanticParentList[0].kind == CXCursorKind.classDecl ||
+                semanticParentList[0].kind == CXCursorKind.structDecl) {
+                    stmt(format(`%s%s kleeObject`, namespaceString, semanticParentList[0].spelling));
+                    stmt(format(`%sm_%s m_kleeObject`, mutNamespaceString, semanticParentList[0].spelling));
+                    namespaceString = ".";
+                    mutNamespaceString = ".";
+                    lastObject = "kleeObject";
+                    lastMutObject = "m_kleeObject";
+                }
             }
 
-            with (else_) {
-                stmt(`klee_assert(0)`);
-                return_(`0`);
-            }
+            if(isFunctionVoid){
+                //Only intended for pure void-functions
+                string mut_params;
+                for (int i = 0; i < params.length; i++) {
+                    stmt(format(`%s m_var%s;`, params[i], i));
+                    mut_params = mut_params ~ format(`m_var%s,`, i);
+                }
+                if (mut_params.length != 0) {
+                    mut_params = mut_params[0 .. $ - 1];
+                }
 
+                // symbolic variable for each parameter
+                string ifstring;
+                for (int i = 0; i < params.length; i++) {
+                    stmt(format(`klee_make_symbolic(&m_var%s, sizeof(%s), "m_var%s")`, i, params[i], i));
+                    ifstring ~= format(`var%s == m_var%s`, i, i);
+                    if(i != params.length-1){
+                        ifstring ~= " && ";
+                    }
+                }
+                with(if_(ifstring)){
+                    stmt(format(`%s%s%s(%s)`, lastObject, namespaceString, function_name, func_params));
+                    stmt(format(`%s%sm_%s(%s)`, lastMutObject, namespaceString, function_name, mut_params));
+                    with(if_(ifstring)){
+                        return_(`0`);
+                    }
+                    with (else_) {
+                        stmt(`klee_assert(0)`);
+                        return_(`0`);
+                    }
+                }
+            }
+            else{
+                with (if_(format(`%s%s%s(%s) == %s%sm_%s(%s)`, lastObject, namespaceString, function_name, func_params,
+                            lastMutObject, mutNamespaceString, function_name, func_params))){
+                        return_(`0`);
+                }
+                with (else_) {
+                    stmt(`klee_assert(0)`);
+                    return_(`0`);
+                }
+            }
         }
-        return code.render;
     }
 }
