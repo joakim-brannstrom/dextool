c3d559c646cecd31c7405dffe041830dacdd8556
Joakim Brannstrom <joakim.brannstrom@gmx.com>
2018-11-21 19:42:33 +0100

    mutate: refactor analyzer

--- plugin/mutate/source/dextool/plugin/mutate/backend/analyzer.d
+++ plugin/mutate/source/dextool/plugin/mutate/backend/analyzer.d
@@ -15,60 +15,83 @@ module dextool.plugin.mutate.backend.analyzer;
 
 import logger = std.experimental.logger;
 
-import dextool.compilation_db : CompileCommandFilter, defaultCompilerFlagFilter,
-    CompileCommandDB;
+import dextool.compilation_db : CompileCommandFilter, defaultCompilerFlagFilter, CompileCommandDB;
 import dextool.set;
 import dextool.type : ExitStatusType, AbsolutePath, Path, DirName;
 import dextool.user_filerange;
 
 import dextool.plugin.mutate.backend.database : Database;
 import dextool.plugin.mutate.backend.interface_ : ValidateLoc, FilesysIO;
-import dextool.plugin.mutate.backend.utility : checksum, trustedRelativePath,
-    Checksum;
+import dextool.plugin.mutate.backend.utility : checksum, trustedRelativePath, Checksum;
 import dextool.plugin.mutate.backend.visitor : makeRootVisitor;
 
 /** Analyze the files in `frange` for mutations.
  */
 ExitStatusType runAnalyzer(ref Database db, ref UserFileRange frange,
         ValidateLoc val_loc, FilesysIO fio) @safe {
-    import std.algorithm : map;
-    import std.path : relativePath;
-    import std.typecons : Yes;
+    auto analyzer = Analyzer(db, val_loc, fio);
+
+    foreach (in_file; frange)
+        analyzer.process(in_file);
+    analyzer.finalize;
+
+    return ExitStatusType.Ok;
+}
+
+private:
+
+struct Analyzer {
+    import std.typecons : NullableRef, Nullable, Yes;
     import cpptooling.analyzer.clang.context : ClangContext;
     import cpptooling.utility.virtualfilesystem;
-    import dextool.clang : findFlags;
-    import dextool.plugin.mutate.backend.type : Language;
+    import dextool.compilation_db : SearchResult;
     import dextool.type : FileName, Exists, makeExists;
     import dextool.utility : analyzeFile;
 
-    // they are not by necessity the same.
-    // Input could be a file that is excluded via --restrict but pull in a
-    // header-only library that is allowed to be mutated.
-    Set!AbsolutePath analyzed_files;
-    Set!AbsolutePath files_with_mutations;
+    private {
+        // they are not by necessity the same.
+        // Input could be a file that is excluded via --restrict but pull in a
+        // header-only library that is allowed to be mutated.
+        Set!AbsolutePath analyzed_files;
+        Set!AbsolutePath files_with_mutations;
 
-    Set!Path before_files = db.getFiles.setFromList;
-    db.removeAllFiles;
+        Set!Path before_files;
 
-    foreach (in_file; frange) {
+        NullableRef!Database db;
+
+        ValidateLoc val_loc;
+        FilesysIO fio;
+    }
+
+    this(ref Database db, ValidateLoc val_loc, FilesysIO fio) @trusted {
+        this.db = &db;
+        this.before_files = db.getFiles.setFromList;
+        this.val_loc = val_loc;
+        this.fio = fio;
+
+        db.removeAllFiles;
+    }
+
+    void process(Nullable!SearchResult in_file) @safe {
+        if (in_file.isNull)
+            return;
+
         // find the file and flags to analyze
-
         Exists!AbsolutePath checked_in_file;
         try {
             checked_in_file = makeExists(in_file.absoluteFile);
         } catch (Exception e) {
             logger.warning(e.msg);
-            continue;
+            return;
         }
 
-        if (analyzed_files.contains(checked_in_file)) {
-            continue;
-        }
+        if (analyzed_files.contains(checked_in_file))
+            return;
 
         analyzed_files.add(checked_in_file);
 
         // analyze the file
-        () @trusted{
+        () @trusted {
             auto ctx = ClangContext(Yes.useInternalHeaders, Yes.prependParamSyntaxOnly);
             auto root = makeRootVisitor(fio, val_loc);
             analyzeFile(checked_in_file, in_file.cflags, root.visitor, ctx);
@@ -96,13 +119,11 @@ ExitStatusType runAnalyzer(ref Database db, ref UserFileRange frange,
         }();
     }
 
-    db.removeOrphanedMutants;
-    printPrunedFiles(before_files, files_with_mutations, fio.getOutputDir);
-
-    return ExitStatusType.Ok;
+    void finalize() @safe {
+        db.removeOrphanedMutants;
+        printPrunedFiles(before_files, files_with_mutations, fio.getOutputDir);
+    }
 }
-
-private:
 
 enum FileStatus {
     noChange,
