0ee47185270c33435ef7ee3f1cf16851652f11f2
nils-petersson <nils.h.petersson@gmail.com>
2018-11-28 15:50:57 +0100

    eqdetect: using nameprefix instead of m_ + some cleanup

--- plugin/eqdetect/source/dextool/plugin/eqdetect/backend/mutation_handler.d
+++ plugin/eqdetect/source/dextool/plugin/eqdetect/backend/mutation_handler.d
@@ -15,7 +15,7 @@ the result before marking the mutation in the database.
 module dextool.plugin.eqdetect.backend.mutation_handler;
 
 import dextool.plugin.eqdetect.backend.visitor : TUVisitor;
-import dextool.plugin.eqdetect.backend.type : Mutation, ErrorResult;
+import dextool.plugin.eqdetect.backend.type : Mutation, ErrorResult, NAME_PREFIX;
 import logger = std.experimental.logger;
 import dextool.plugin.mutate.backend.type : Offset;
 
@@ -23,26 +23,30 @@ const string SOURCE_TYPE = "_source_";
 const string MUTANT_TYPE = "_mutant_";
 const string KLEE_TYPE = "_klee_";
 
-void handleMutation(TUVisitor visitor, Mutation mutation) {
-    // create file for sourcecode, the mutant and code prepared for KLEE
+string muttext;
+string sourcetext;
+
+void handleMutation(TUVisitor visitor, Mutation mutation, const string[] cflags) {
+    // change names in the mutantfile
     mutationEnhancer(visitor, mutation);
+    // create file for sourcecode, the mutant and code prepared for KLEE
     createSourceFiles(visitor);
     // start the symbolic execution in KLEE
-    runKLEE();
+    runKLEE(cflags);
     // parse the result
     ErrorResult errorResult = parseKLEE();
     // mark the mutation in the database according to KLEE output
     markMutation(errorResult, mutation);
 }
 
-Offset updateMutationOffset(Offset visitorOffset, Offset offset){
+Offset updateMutationOffset(Offset visitorOffset, Offset offset, int diff){
     if (offset.begin > visitorOffset.begin
             && offset.end > visitorOffset.end) {
-        offset.begin += 2;
-        offset.end += 2;
+        offset.begin += diff;
+        offset.end += diff;
     } else if (offset.begin <= visitorOffset.begin
             && offset.end >= visitorOffset.end) {
-        offset.end += 2;
+        offset.end += diff;
     }
     return offset;
 }
@@ -50,26 +54,47 @@ Offset updateMutationOffset(Offset visitorOffset, Offset offset){
 void mutationEnhancer(TUVisitor visitor, Mutation mutation) {
     import std.algorithm;
     import std.stdio;
+    import std.path : stripExtension, extension;
+    import std.conv : to;
+
     visitor.offsets.sort!("a.begin > b.begin");
     visitor.headerOffsets.sort!("a.begin > b.begin");
+
+    FileName source_path = createFilename(visitor, SOURCE_TYPE, ".hpp");
+
     string text = visitor.generatedSource.render;
+
+    if(visitor.includeOffset.begin != -1){
+        int headerDiff = to!int(("\"" ~ source_path ~ "\"").length) - (visitor.includeOffset.end - visitor.includeOffset.begin);
+        text = visitor.generatedSource.render[0 .. visitor.includeOffset.begin] ~ "\"" ~
+        stripExtension(source_path)~".hpp" ~ "\"" ~ visitor.generatedSource.render[visitor.includeOffset.end .. $];
+
+        for (int i = 0; i < visitor.offsets.length; i++) {
+            visitor.offsets[i] = updateMutationOffset(visitor.includeOffset, visitor.offsets[i], headerDiff);
+        }
+        mutation.offset = updateMutationOffset(visitor.includeOffset, mutation.offset, headerDiff);
+
+    }
+    sourcetext = text;
+
     string text2 = visitor.generatedSourceHeader.render;
     for (int i = 0; i < visitor.offsets.length; i++) {
         text = nameReplacer(text, visitor.offsets[i]);
-        mutation.offset = updateMutationOffset(visitor.offsets[i], mutation.offset);
+        mutation.offset = updateMutationOffset(visitor.offsets[i], mutation.offset, NAME_PREFIX.length);
     }
     for (int i = 0; i < visitor.headerOffsets.length; i++) {
         text2 = nameReplacer(text2, visitor.headerOffsets[i]);
     }
 
-    import dextool.plugin.eqdetect.backend.codegenerator : SnippetFinder;
+    import dextool.plugin.eqdetect.backend.codegenerator;
 
-    visitor.generatedMutation.text(SnippetFinder.generateMut(text, mutation));
+    visitor.generatedMutation.text(generateMut(text, mutation));
+    muttext = visitor.generatedMutation.render;
     visitor.generatedMutationHeader.text(text2);
 }
 
 string nameReplacer(string text, Offset offset) {
-    text = text[0 .. offset.begin] ~ "m_" ~ text[offset.begin .. $];
+    text = text[0 .. offset.begin] ~ NAME_PREFIX ~ text[offset.begin .. $];
     return text;
 }
 
@@ -89,47 +114,36 @@ FileName createFilename(TUVisitor visitor, string filetype, string extension){
 
 void createSourceFiles(TUVisitor visitor) {
     import std.path : extension, stripExtension;
-    import dextool.plugin.eqdetect.backend : writeToFile, SnippetFinder;
+    import dextool.plugin.eqdetect.backend : generateKlee, writeToFile;
     import dextool.type : FileName;
 
     FileName source_path = createFilename(visitor, SOURCE_TYPE, extension(visitor.mutation.path));
     FileName mutant_path = createFilename(visitor, MUTANT_TYPE, extension(visitor.mutation.path));
 
-    string muttext;
-    string sourcetext;
-
-    if (visitor.includeOffset.begin != -1 && visitor.includeOffset.end != -1){
-        muttext = visitor.generatedMutation.render[0 .. visitor.includeOffset.begin] ~ "\"" ~
-        stripExtension(mutant_path)~".hpp" ~ "\"" ~ visitor.generatedMutation.render[visitor.includeOffset.end .. $];
-        sourcetext = visitor.generatedSource.render[0 .. visitor.includeOffset.begin] ~ "\"" ~
-        stripExtension(source_path)~".hpp" ~ "\"" ~ visitor.generatedSource.render[visitor.includeOffset.end .. $];
-    } else {
-        muttext = visitor.generatedMutation.render;
-        sourcetext = visitor.generatedSource.render;
-    }
+    import std.array : replace;
+    import std.conv : to;
+    muttext = muttext.replace(to!string(stripExtension(source_path)), to!string(stripExtension(mutant_path)));
 
     writeToFile(sourcetext, source_path);
     writeToFile(muttext, mutant_path);
 
-    auto s = SnippetFinder.generateKlee(visitor.function_params, source_path,
-            mutant_path, visitor.function_name, visitor.semanticParentList, visitor.isFunctionVoid);
+    auto s = generateKlee(visitor.entryFunction, source_path, mutant_path);
 
     writeToFile(s, createFilename(visitor, KLEE_TYPE, extension(visitor.mutation.path)));
 
-    import std.array : replace;
     string headerExtension = extension(visitor.mutation.path).replace("c", "h");
 
     source_path = createFilename(visitor, SOURCE_TYPE, headerExtension);
     mutant_path = createFilename(visitor, MUTANT_TYPE, headerExtension);
 
     //Can't find #ifndef when analyzing the AST, working for now
-    muttext = (visitor.generatedMutationHeader.render).replace("#ifndef ", "#ifndef m_");
+    muttext = (visitor.generatedMutationHeader.render).replace("#ifndef ", "#ifndef " ~ NAME_PREFIX);
 
     writeToFile(visitor.generatedSourceHeader.render, source_path);
     writeToFile(muttext, mutant_path);
 }
 
-void runKLEE() {
+void runKLEE(const string[] cflags) {
     import std.process : executeShell;
     import std.file : getcwd;
     import std.format : format;
@@ -137,7 +151,9 @@ void runKLEE() {
     logger.info("KLEE execution started");
 
     //TODO: get rid of klee.sh
-    logger.info(executeShell("./klee.sh").output);
+    import std.string;
+    string incPath = cflags[2].strip("-I");
+    logger.info(executeShell("./klee.sh " ~ incPath).output);
 
     // cleanup the temporary directory created
     executeShell("rm -rf eqdetect_generated_files/*");
@@ -148,7 +164,6 @@ ErrorResult parseKLEE() {
     import dextool.plugin.eqdetect.backend.parser : errorTextParser;
 
     ErrorResult errorResult = errorTextParser("result.txt");
-
     // remove the result-file
     import std.file : remove;
 
