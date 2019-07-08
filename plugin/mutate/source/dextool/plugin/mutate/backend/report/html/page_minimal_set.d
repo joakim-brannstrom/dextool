c62dd6f28664185c682f85717e2cb68d22856561
lisahabbe <lisa.habbe@gmail.com>
2019-07-08 08:56:25 +0200

    tables minimizes in minimal set

--- plugin/mutate/source/dextool/plugin/mutate/backend/report/html/page_minimal_set.d
+++ plugin/mutate/source/dextool/plugin/mutate/backend/report/html/page_minimal_set.d
@@ -12,11 +12,13 @@ module dextool.plugin.mutate.backend.report.html.page_minimal_set;
 import logger = std.experimental.logger;
 import std.format : format;
 
-import arsd.dom : Document, Element, require, Table;
+import arsd.dom : Document, Element, require, Table, RawSource;
 
 import dextool.plugin.mutate.backend.database : Database;
 import dextool.plugin.mutate.backend.report.html.constants;
-import dextool.plugin.mutate.backend.report.html.tmpl : tmplBasicPage, tmplDefaultTable;
+import dextool.plugin.mutate.backend.report.html.js;
+import dextool.plugin.mutate.backend.report.html.tmpl : tmplBasicPage,
+    tmplDefaultTable, tmplDefaultMatrixTable;
 import dextool.plugin.mutate.backend.report.utility;
 import dextool.plugin.mutate.backend.type : Mutation;
 import dextool.plugin.mutate.config : ConfigReport;
@@ -27,9 +29,18 @@ auto makeMinimalSetAnalyse(ref Database db, ref const ConfigReport conf,
     import std.datetime : Clock;
 
     auto doc = tmplBasicPage;
-    doc.title(format("Minimal Set Analyse %(%s %) %s", humanReadableKinds, Clock.currTime));
+
+    auto s = doc.root.childElements("head")[0].addChild("script");
+    s.addChild(new RawSource(doc, js_similarity));
+
+
+    doc.title(format("Minimal Set Analyse %(%s %) %s", 
+        humanReadableKinds, Clock.currTime));
+    doc.mainBody.setAttribute("onload", "init()");
     doc.mainBody.addChild("p",
             "This are the minimal set of mutants that result in the mutation score.");
+
+         auto p = doc.mainBody.addChild("p");
 
     toHtml(reportMinimalSet(db, kinds), doc.mainBody);
 
@@ -41,14 +52,20 @@ private:
 void toHtml(MinimalTestSet min_set, Element root) {
     import core.time : Duration;
     import std.conv : to;
-
-    root.addChild("h2", format!"Ineffective Test Cases (%s/%s %s)"(min_set.redundant.length,
-            min_set.total, cast(double) min_set.redundant.length / cast(double) min_set.total));
-    root.addChild("p", "These test cases do not contribute towards the mutation score.");
     {
-        auto tbl = tmplDefaultTable(root, [
+        root.addChild("p", "The tables are hidden by default, click on the corresponding header to display a table.");
+
+        auto comp_container = root.addChild("div").addClass("comp_container");
+        comp_container.addChild("h2",format!"Ineffective Test Cases (%s/%s %s)"(min_set.redundant.length,
+                min_set.total, cast(double) min_set.redundant.length / cast(double) min_set.total)).addClass("tbl_header");
+        comp_container.addChild("p", "These test cases do not contribute towards the mutation score.");
+
+        auto tbl_container = comp_container.addChild("div").addClass("tbl_container");
+        tbl_container.setAttribute("style", "display: none;");
+        auto tbl = tmplDefaultTable(tbl_container, [
                 "Test Case", "Killed", "Sum of test time"
                 ]);
+
         Duration sum;
         foreach (const tc; min_set.redundant) {
             auto r = tbl.appendRow();
@@ -57,16 +74,20 @@ void toHtml(MinimalTestSet min_set, Element root) {
             r.addChild("td", min_set.testCaseTime[tc.name].time.to!string);
             sum += min_set.testCaseTime[tc.name].time;
         }
-        root.addChild("p", format("Total test time: %s", sum));
+        tbl_container.addChild("p", format("Total test time: %s", sum));
     }
+    { 
+        auto comp_container = root.addChild("div").addClass("comp_container");
+        comp_container.addChild("h2", format!"Minimal Set (%s/%s %s)"(min_set.minimalSet.length,
+                min_set.total, cast(double) min_set.minimalSet.length / cast(double) min_set.total)).addClass("tbl_header");
+        comp_container.addChild("p", "This is the minimum set of tests that achieve the mutation score.");
 
-    root.addChild("h2", format!"Minimal Set (%s/%s %s)"(min_set.minimalSet.length,
-            min_set.total, cast(double) min_set.minimalSet.length / cast(double) min_set.total));
-    root.addChild("p", "This is the minimum set of tests that achieve the mutation score.");
-    {
-        auto tbl = tmplDefaultTable(root, [
+        auto tbl_container = comp_container.addChild("div").addClass("tbl_container");
+        tbl_container.setAttribute("style", "display: none;");
+        auto tbl = tmplDefaultTable(tbl_container, [
                 "Test Case", "Killed", "Sum of test time"
                 ]);
+
         Duration sum;
         foreach (const tc; min_set.minimalSet) {
             auto r = tbl.appendRow();
@@ -75,6 +96,6 @@ void toHtml(MinimalTestSet min_set, Element root) {
             r.addChild("td", min_set.testCaseTime[tc.name].time.to!string);
             sum += min_set.testCaseTime[tc.name].time;
         }
-        root.addChild("p", format("Total test time: %s", sum));
+        tbl_container.addChild("p", format("Total test time: %s", sum));
     }
 }
