dc9308b8d4a2685bb4ee384efc7f6ad7e0a989d5
Eric <eric.lindskog25@gmail.com>
2019-07-15 10:57:03 +0200

    JSON for treemap is now added to the html instead, as a separate file caused problems with newer browsers. Cleaned up the creation of the JSON, still a bit messy though.

--- plugin/mutate/source/dextool/plugin/mutate/backend/report/html/page_tree_map.d
+++ plugin/mutate/source/dextool/plugin/mutate/backend/report/html/page_tree_map.d
@@ -7,61 +7,60 @@ import dextool.plugin.mutate.backend.report.html.tmpl : tmplBasicPage;
 
 @trusted auto makeTreeMapJSON(FileIndex[] files) {
     import std.stdio : writeln;
-    import std.array : array, split;
+    import std.array : array;
     import std.range : enumerate;
     import std.json : JSONValue;
+    import std.path : pathSplitter;
+    import std.format;
 
     JSONValue root = ["name" : "root"];
-    auto curr = root;
+    auto parent = root;
     foreach (f; files) {
-        auto path = f.display.split("/");
+        auto path = pathSplitter(f.display);
 
-        foreach (i, seg; path.enumerate) {
-            if ("children" in curr) {
-                bool exists = false;
-                for (int j; j < curr["children"].array.length; j++) {
-                    auto c = curr["children"][j];
-
-                    if (c["name"].str == seg) {
-                        curr = c;
-                        exists = true;
-                        break;
+        segments: foreach (i, seg; path.enumerate) {
+            if ("children" in parent) { // Parent has children -> look for segment in children
+                for (int j; j < parent["children"].array.length; j++) {
+                    auto child = parent["children"][j];
+                    
+                    if (child["name"].str == seg) {// Segment already added to JSON
+                        parent = child; // Update parent
+                        continue segments; // Go to next segment
                     }
                 }
-                if (exists)
-                    continue;
+            } 
+            else { // Parent has no children -> init list
+                parent.object["children"] = JSONValue[].init;
             }
-            JSONValue tmp = ["name" : seg];
-
-            if (i + 1 == path.length) {
-                tmp.object["locs"] = JSONValue(f.totalMutants);
-                if (f.totalMutants == 0)
-                    tmp.object["score"] = JSONValue(1.0);
-                else
-                    tmp.object["score"] = JSONValue(
-                            cast(double) f.killedMutants / cast(double) f.totalMutants);
-            }
-            if ("children" !in curr) {
-                curr.object["children"] = JSONValue([tmp]);
-            } else {
-                curr.object["children"].array ~= tmp;
-            }
-            curr = tmp;
-
+            
+            JSONValue child = ["name": seg]; // Create new child
+            parent.object["children"].array ~= child; // Add to parent
+            parent = child; // Update parent          
         }
-        curr = root;
+
+        // Reached final segment -> file. Add score and locs
+        parent.object["locs"] = JSONValue(f.totalMutants);
+        if (f.totalMutants == 0) {
+            parent.object["score"] = JSONValue(1.0);
+        } else {
+            parent.object["score"] = JSONValue(
+                    cast(double) f.killedMutants / cast(double) f.totalMutants);
+        }
+        parent = root; //Finished with file -> set parent to root.
     }
     return root.toPrettyString;
 }
 
-auto makeTreeMapPage() {
+auto makeTreeMapPage(FileIndex[] files) {
+    import std.format : format;
     auto doc = tmplBasicPage;
 
-    //doc.mainBody.setAttribute("onload", "init()");
+    doc.mainBody.setAttribute("onload", "init()");
     auto s = doc.root.childElements("head")[0].addChild("script");
     s.addChild(new RawSource(doc, import("d3.min.js")));
     auto container = doc.mainBody.addChild("div");
     auto s2 = doc.mainBody.addChild("script");
+    s2.addChild(new RawSource(doc, format("const g_indata = %s", makeTreeMapJSON(files))));
     s2.addChild(new RawSource(doc, import("treemap.js")));
     container.setAttribute("id", "container");
     return doc.toPrettyString;
