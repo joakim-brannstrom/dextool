c0b96c183ed741fe8b3122b393b744de93d3e90d
Eric <eric.lindskog25@gmail.com>
2019-07-16 15:45:04 +0200

    page_tree_map.d : Moved public functions to the top. Made changes to JSONLike to make it more easily understandable, made the binary operator overload cause compilation error if non-implemented operators are used

--- plugin/mutate/source/dextool/plugin/mutate/backend/report/html/page_tree_map.d
+++ plugin/mutate/source/dextool/plugin/mutate/backend/report/html/page_tree_map.d
@@ -11,61 +11,37 @@ import dextool.plugin.mutate.backend.report.html.tmpl : tmplBasicPage;
 * Has operator overloads for "index", "index assign" and "in" allowing the children list to be private
 * and cleaner construction of the object. 
 */
-class JSONLike {
-    string name;
-    private JSONLike[string] children;
-    long locs = -1;
-    double score = -1;
+auto makeTreeMapPage(FileIndex[] files) {
+    import std.format : format;
 
-    JSONValue toJSONValue() {
-        JSONValue root = ["name" : name];
+    auto doc = tmplBasicPage;
+    doc.mainBody.setAttribute("onload", "init()");
 
-        if (children.length) {
-            JSONValue[] descendants = [];
-            foreach (c; children) {
-                descendants ~= c.toJSONValue();
-            }
-            root.object["children"] = JSONValue(descendants);
-        }
-        if (locs != -1)
-            root.object["locs"] = JSONValue(locs);
+    auto s = doc.root.childElements("head")[0].addChild("script");
+    s.addChild(new RawSource(doc, import("d3.min.js")));
 
-        if (score != -1)
-            root.object["score"] = JSONValue(score);
+    auto container = doc.mainBody.addChild("div");
+    container.setAttribute("id", "container");
 
-        return root;
-    }
+    auto s2 = doc.mainBody.addChild("script");
+    s2.addChild(new RawSource(doc, format("const g_indata = %s", makeTreeMapJSON(files))));
+    s2.addChild(new RawSource(doc, import("treemap.js")));
 
-    JSONLike opIndex(string i) pure nothrow {
-        return children[i];
-    }
-
-    JSONLike opIndexAssign(JSONLike value, string i) pure nothrow {
-        children[i] = value;
-        return value;
-    }
-
-    bool opBinaryRight(string op)(string a) {
-        if (op == "in") {
-            if (a in children)
-                return true;
-            return false;
-        }
-        return false;
-    }
-
+    return doc.toPrettyString;
 }
 
-@trusted auto makeTreeMapJSON(FileIndex[] files) {
+private:
+
+auto makeTreeMapJSON(FileIndex[] files) {
     import std.array : array;
     import std.path : pathSplitter;
 
-    auto iRoot = new JSONLike();
-    iRoot.name = "root";
+    auto root = new JSONLike();
+    root.name = "root";
 
     foreach (f; files) {
         auto path = pathSplitter(f.display);
-        auto parent = iRoot;
+        auto parent = root;
         foreach (seg; path) {
             if (seg !in parent) {
                 auto child = new JSONLike();
@@ -80,21 +56,53 @@ class JSONLike {
         else
             parent.score = cast(double) f.killedMutants / cast(double) f.totalMutants;
     }
-    return iRoot.toJSONValue().toPrettyString();
+    return root.toJSONValue().toPrettyString();
 }
 
-auto makeTreeMapPage(FileIndex[] files) {
-    import std.format : format;
+class JSONLike {
+    import std.typecons : Nullable;
+    import std.array : empty;
 
-    auto doc = tmplBasicPage;
-    doc.mainBody.setAttribute("onload", "init()");
-    auto s = doc.root.childElements("head")[0].addChild("script");
-    s.addChild(new RawSource(doc, import("d3.min.js")));
-    auto container = doc.mainBody.addChild("div");
-    auto s2 = doc.mainBody.addChild("script");
-    s2.addChild(new RawSource(doc, format("const g_indata = %s", makeTreeMapJSON(files))));
-    s2.addChild(new RawSource(doc, import("treemap.js")));
-    container.setAttribute("id", "container");
+    string name;
+    private JSONLike[string] children;
+    Nullable!long locs;
+    Nullable!double score;
 
-    return doc.toPrettyString;
+    JSONValue toJSONValue() {
+        JSONValue root = ["name" : name];
+
+        if (!children.empty) {
+            JSONValue[] descendants = [];
+            foreach (c; children) {
+                descendants ~= c.toJSONValue();
+            }
+            root.object["children"] = JSONValue(descendants);
+        }
+        if (!locs.isNull)
+            root.object["locs"] = JSONValue(locs.get);
+
+        if (!score.isNull)
+            root.object["score"] = JSONValue(score.get);
+
+        return root;
+    }
+
+    JSONLike opIndex(string i) pure nothrow {
+        return children[i];
+    }
+
+    JSONLike opIndexAssign(JSONLike value, string i) pure nothrow {
+        children[i] = value;
+        return value;
+    }
+
+    bool opBinaryRight(string op)(string a) {
+        static if (op == "in") {
+            if (a in children) {
+                return true;
+            }
+            return false;
+        } else
+            static assert(0, "Operator " ~ op ~ " not implmeneted");
+    }
 }
