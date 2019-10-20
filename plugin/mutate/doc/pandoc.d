31572f5f36d8e6c339958b518eeea87be9520519
Joakim Brannstrom <joakim.brannstrom@gmx.com>
2019-10-20 17:42:10 +0200

    mutate: (doc) update pdf generator

--- plugin/mutate/doc/pandoc.d
+++ plugin/mutate/doc/pandoc.d
@@ -6,21 +6,21 @@
  * documentation for the Dextool mutation testing plugin.
  *
  * Dependent on the following packages (Ubuntu):
- * `sudo apt install texlive-bibtex-extra biber pandoc pandoc-citeproc`
+ * `sudo apt install texlive-bibtex-extra biber pandoc pandoc-citeproc texlive-font-utils latexmk`
  */
-
+import core.stdc.stdlib;
+import logger = std.experimental.logger;
 import std.algorithm;
 import std.array;
 import std.ascii;
 import std.conv;
+import std.exception : collectException;
 import std.file;
-import std.process;
 import std.path;
-import core.stdc.stdlib;
+import std.process;
 import std.range;
 import std.stdio;
 import std.string;
-import logger = std.experimental.logger;
 
 void main(string[] args) {
     const string root = getcwd();
@@ -29,6 +29,8 @@ void main(string[] args) {
     if (!exists(latex_dir)) {
         mkdir(latex_dir);
     }
+
+    prepareFigures(buildPath(root, "design", "figures"), buildPath(latex_dir, "figures"));
 
     chdir(latex_dir);
     scope (exit)
@@ -41,29 +43,17 @@ void main(string[] args) {
 
     auto dat = Pandoc(metadata, latex_template, biblio);
 
-    string[] design_chapters;
-    design_chapters ~= "use_cases.md";
-    design_chapters ~= "purpose.md";
-    design_chapters ~= "security.md";
-    design_chapters ~= "architecture.md";
-    design_chapters ~= "mutations.md";
-    design_chapters ~= "analyzer/analyzer.md";
-    design_chapters ~= "test_mutant/basis.md";
-    design_chapters ~= "usability/sanity_check.md";
-    design_chapters ~= "usability/report.md";
-    design_chapters ~= "future_work.md";
+    string[] chapters = [
+        "use_cases.md", "purpose.md", "security.md", "architecture.md",
+        "mutations.md", "analyzer/analyzer.md", "test_mutant/basis.md",
+        "usability/sanity_check.md", "usability/report.md", "future_work.md",
+    ];
 
-    // dfmt off
-    pandoc(
-        dat,
-        chain(design_chapters.map!(a => buildPath(root, "design", a)),
-              ["definitions.md", "abbrevations.md", "appendix.md", "references.md"].map!(a => buildPath(root, a))).array,
-        output);
-    // dfmt on
-}
-
-string home() {
-    return expandTilde("~");
+    pandoc(dat, chain(chapters.map!(a => buildPath(root, "design", a)),
+            [
+                "definitions.md", "abbrevations.md", "appendix.md",
+                "references.md"
+            ].map!(a => buildPath(root, a))).array, output);
 }
 
 struct Pandoc {
@@ -73,36 +63,60 @@ struct Pandoc {
 }
 
 void pandoc(Pandoc dat, string[] files, const string output) {
+    const outputTex = output ~ ".tex";
+    const biblio = buildPath(output.dirName, dat.biblio.baseName);
+    copy(dat.biblio, biblio);
+
     // dfmt off
     auto cmd = ["pandoc",
+         //"--pdf-engine", "xelatex",
          "--template", dat.latexTemplate,
-         "-f", "markdown_github+citations+yaml_metadata_block+tex_math_dollars+raw_tex+smart",
+         "-f", "markdown+smart+pipe_tables+raw_html+fenced_code_blocks+auto_identifiers+gfm_auto_identifiers+backtick_code_blocks+autolink_bare_uris+space_in_atx_header+strikeout+shortcut_reference_links+angle_brackets_escapable+lists_without_preceding_blankline+citations+yaml_metadata_block+tex_math_dollars+raw_tex+footnotes+header_attributes+link_attributes",
+         "-t", "latex",
          "--standalone",
          "--toc",
-         "--bibliography", dat.biblio,
+         "--bibliography", biblio,
          //"--biblio", dat.biblio,
          //"--biblatex", "-M", "biblio-style=numeric-comp",
          //"--csl", "chicago-author-date.csl",
          "--natbib", "-M", "biblio-style=unsrtnat", "-M", "biblio-title=heading=none",
-         //"--to", "latex",
-         //"-o", output ~ ".pdf",
-         "-o", output ~ ".latex",
+         "-o", outputTex,
          dat.metadata,
     ] ~ files;
     // dfmt on
 
     run(cmd);
-    // generates the aux
-    run(["pdflatex", output ~ ".latex"]);
-    // generate first pass of the resolution of references
-    try {
-        run(["bibtex", output ~ ".aux"]);
-    } catch (Exception e) {
-    }
+    run(["pdflatex", outputTex]);
+    run(["bibtex", outputTex.setExtension("aux")]).collectException;
+    run(["pdflatex", outputTex]);
+    run(["pdflatex", outputTex]);
+}
 
-    // continue resolving references. At least two times is needed.
-    run(["pdflatex", output ~ ".latex"]);
-    run(["pdflatex", output ~ ".latex"]);
+void prepareFigures(string src, string dest_dir) {
+    import std.algorithm : map, joiner;
+    import std.datetime : SysTime, Clock;
+    import std.file : timeLastModified, setTimes;
+    static import std.process;
+
+    if (!exists(dest_dir))
+        mkdirRecurse(dest_dir);
+
+    foreach (f; dirEntries(src, SpanMode.shallow).filter!(a => a.extension.among(".pu", ".uml"))) {
+        const dst = buildPath(dest_dir, f.name.baseName);
+        if (f.timeLastModified < timeLastModified(dst, SysTime.min)) {
+            writefln("%s not modified thus skipping to update the image", f);
+            continue;
+        }
+
+        writefln("Generating image %s -> %s", f, dst.setExtension("eps"));
+        copy(f.name, dst);
+        setTimes(dst, Clock.currTime, Clock.currTime);
+        try {
+            spawnProcess(["plantuml", "-teps", dst], null, std.process.Config.none, dest_dir).wait;
+        } catch (Exception e) {
+            logger.warning(e.msg);
+        }
+    }
 }
 
 auto run(string[] cmd) {
