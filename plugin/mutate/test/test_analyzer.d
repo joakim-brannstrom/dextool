9147ae9e3ba2e46387e0641a213ff13b1fa7d0e9
Joakim Brannstrom <joakim.brannstrom@gmx.com>
2019-11-03 19:24:17 +0100

    mutate: add test case for excluding files from analysis

--- plugin/mutate/test/test_analyzer.d
+++ plugin/mutate/test/test_analyzer.d
@@ -5,6 +5,13 @@ Author: Joakim Brännström (joakim.brannstrom@gmx.com)
 */
 module dextool_test.test_analyzer;
 
+import std.path : relativePath;
+
+import dextool.plugin.mutate.backend.database.standalone;
+import dextool.plugin.mutate.backend.database.type;
+import dextool.plugin.mutate.backend.type;
+static import dextool.type;
+
 import dextool_test.utility;
 
 // dfmt off
@@ -15,4 +22,27 @@ unittest {
     makeDextoolAnalyze(testEnv)
         .addInputArg(testData ~ "all_kinds_of_abs_mutation_points.cpp")
         .run;
+}
+
+@(testId ~ "shall exclude files from the analysis they are part of an excluded directory tree when analysing")
+unittest {
+    mixin(EnvSetup(globalTestdir));
+
+    const programFile1 = testData ~ "analyze/file1.cpp";
+    const programFile2 = testData ~ "analyze/exclude/file2.cpp";
+
+    makeDextoolAnalyze(testEnv)
+        .addInputArg(programFile1)
+        .addInputArg(programFile2)
+        .addPostArg(["--file-exclude", (testData ~ "analyze/exclude").toString])
+        .run;
+
+    // assert
+    auto db = Database.make((testEnv.outdir ~ defaultDb).toString);
+
+    const file1 = dextool.type.Path(relativePath(programFile1.toString, workDir.toString));
+    const file2 = dextool.type.Path(relativePath(programFile2.toString, workDir.toString));
+
+    db.getFileId(file1).isNull.shouldBeFalse;
+    db.getFileId(file2).isNull.shouldBeTrue;
 }
---
+++ plugin/mutate/testdata/analyze/file1.cpp
@@ -0,0 +1,10 @@
+/// @copyright Boost License 1.0, http://boost.org/LICENSE_1_0.txt
+/// @date 2019
+/// @author Joakim Brännström (joakim.brannstrom@gmx.com)
+
+int fun(int x) {
+    if (x > 3) {
+        return 0;
+    }
+    return 1;
+}
