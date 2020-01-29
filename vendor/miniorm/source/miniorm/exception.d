2b0a56fd18d9245a39ee7bd34415c521327d51d9
Joakim Brannstrom <joakim.brannstrom@gmx.com>
2020-01-29 21:53:28 +0100

    miniorm: change the API to only allow binding of values
    No textual insert.

--- vendor/miniorm/source/miniorm/exception.d
+++ vendor/miniorm/source/miniorm/exception.d
@@ -1,7 +1,12 @@
 ///
 module miniorm.exception;
 
+import logger = std.experimental.logger;
+
 import d2sqlite3;
+
+import miniorm.api : RefCntStatement;
+import miniorm.queries : Bind;
 
 class MiniormException : Exception {
     SqliteException sqliteException;
@@ -13,16 +18,26 @@ class MiniormException : Exception {
     }
 }
 
-auto executeCheck(ref Database db, string queryStr) {
-    try
-        return db.execute(queryStr);
-    catch (SqliteException e)
-        throw new MiniormException(queryStr, e);
+class MiniormBindException : Exception {
+    this(string fq, size_t actualBind, size_t expectedBind) {
+        import std.format : format;
+
+        super(format!"Expected %s parameters but %s provided\nfull query: %s"(expectedBind,
+                actualBind, fq));
+    }
 }
 
-auto runCheck(ref Database db, string queryStr) {
-    try
-        return db.run(queryStr);
-    catch (SqliteException e)
-        throw new MiniormException(queryStr, e);
+auto executeCheck(Args...)(RefCntStatement stmt, string query, Bind[] binds, auto ref Args args) {
+    if (binds.length != Args.length) {
+        throw new MiniormBindException(query, args.length, binds.length);
+    }
+
+    try {
+        int idx;
+        static foreach (a; args) {
+            stmt.get.bind(binds[idx++].toString, a);
+        }
+        return stmt.get.execute;
+    } catch (SqliteException e)
+        throw new MiniormException(query, e);
 }
