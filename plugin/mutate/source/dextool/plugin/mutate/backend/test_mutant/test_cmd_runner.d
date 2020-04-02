f787a270514b7595d58aff70a8c5b43eed3667b5
Joakim Brannstrom <joakim.brannstrom@gmx.com>
2020-04-02 09:21:04 +0200

    mutate: update comments

--- plugin/mutate/source/dextool/plugin/mutate/backend/test_mutant/test_cmd_runner.d
+++ plugin/mutate/source/dextool/plugin/mutate/backend/test_mutant/test_cmd_runner.d
@@ -162,13 +162,15 @@ struct TestRunner {
         if (nrOfRuns == 0) {
             commands = commands.randomCover.array;
         } else if (nrOfRuns % reorderWhen == 0) {
-            // use those that kill the most first
+            // use a forget factor to make the order re-arrange over time
+            // if the "best" test case change.
             foreach (ref a; commands) {
                 a.kills = a.kills * 0.9;
             }
 
             import std.algorithm : sort;
 
+            // use those that kill the most first
             commands = commands.sort!((a, b) => a.kills > b.kills).array;
             logger.infof("Update test command order: %(%s, %)",
                     commands.take(reorderWhen).map!(a => format("%s:%.2f", a.cmd, a.kills)));
