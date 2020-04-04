5bbd53756915e2f3bf59d1bfd37ba97c9100df1f
Joakim Brannstrom <joakim.brannstrom@gmx.com>
2020-04-04 15:03:45 +0200

    mutate: add tests for reading the active mutant from the environment

--- plugin/mutate/test/test_schemata_c_header.cpp
+++ plugin/mutate/test/test_schemata_c_header.cpp
@@ -4,6 +4,62 @@
 //
 // This test the preamble for mutants that it works as expected
 
+#include <assert.h>
+#include <cstdint>
+#include <cstring>
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+
+#define start_test()                                                                               \
+    do {                                                                                           \
+        std::cout << " # " << __func__ << "\t\t" << __FILE__ << ":" << __LINE__ << std::endl;      \
+    } while (0)
+#define msg(x...)                                                                                  \
+    do {                                                                                           \
+        std::cout << __FILE__ << ":" << __LINE__ << " " << x << std::endl;                         \
+    } while (0)
+
 #include "schemata_header.c"
 
-int main(int argc, char** argv) { return 0; }
+const char* EnvKey = "DEXTOOL_MUTID";
+
+void set_env_mutid(uint64_t v) {
+    auto ss = std::string(EnvKey) + "=" + std::to_string(v);
+    char* s = new char[ss.length() + 1];
+    strcpy(s, ss.c_str());
+    assert(putenv(s) == 0);
+}
+
+void test_id_read() {
+    start_test();
+
+    msg("Setting env to " << 42);
+    set_env_mutid(42);
+
+    msg("Let init_dextool_mutid read from env");
+    init_dextool_mutid();
+
+    msg("global variable gDEXTOOL_MUTID is " << gDEXTOOL_MUTID);
+    assert(gDEXTOOL_MUTID == 42);
+}
+
+void test_read_largest() {
+    start_test();
+
+    msg("Setting the env to the largest possible value");
+    set_env_mutid(UINT64_MAX);
+
+    init_dextool_mutid();
+
+    msg("global variable gDEXTOOL_MUTID is " << gDEXTOOL_MUTID);
+    assert(gDEXTOOL_MUTID == UINT64_MAX);
+}
+
+int main(int argc, char** argv) {
+    assert(getenv(EnvKey) == nullptr);
+
+    test_id_read();
+    test_read_largest();
+    return 0;
+}
