98b1e0eb9d88e1ee1a4ca8c15e9afdf95863a319
Joakim Brannstrom <joakim.brannstrom@gmx.com>
2021-01-11 01:23:14 +0100

    mutate: suppress warning to improve running with -Werror

--- plugin/mutate/data/schemata_header.c
+++ plugin/mutate/data/schemata_header.c
@@ -21,6 +21,7 @@ __attribute__((constructor, weak)) void dextool_init_mutid(void) {
     }
 }
 
+#pragma GCC diagnostic ignored "-Wsuggest-attribute=pure"
 __attribute__((weak)) unsigned int dextool_get_mutid__(void) { return gDEXTOOL_MUTID; }
 
 #endif /* DEXTOOL_MUTANT_SCHEMATA_INCL_GUARD */
