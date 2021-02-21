79a094b8e2b52b49572845de00183e50a385405a
Joakim Brannstrom <joakim.brannstrom@gmx.com>
2021-02-21 21:19:05 +0100

    vendor: update my

--- vendor/mylib/source/my/libc.d
+++ vendor/mylib/source/my/libc.d
@@ -8,4 +8,21 @@ A variaty of libc bindings.
 module my.libc;
 
 // malloc_trim - release free memory from the heap
-extern (C) int malloc_trim(size_t pad) nothrow @system;
+extern (C) int malloc_trim(size_t pad) @nogc nothrow @system;
+
+/**
+DESCRIPTION
+
+     The getloadavg() function returns the number of processes in the system
+     run queue averaged over various periods of time.  Up to nelem samples are
+     retrieved and assigned to successive elements of loadavg[].  The system
+     imposes a maximum of 3 samples, representing averages over the last 1, 5,
+     and 15 minutes, respectively.
+
+
+DIAGNOSTICS
+
+     If the load average was unobtainable, -1 is returned; otherwise, the num-
+     ber of samples actually retrieved is returned.
+ */
+extern (C) int getloadavg(double* loadavg, int nelem) @nogc nothrow @system;
