011212cefe86df20f3fdd749e6f10ff5874671e8
Viktor Norgren <v.norgren01@gmail.com>
2022-06-16 08:10:45 +0200

    Now displays an 'average' line on the graph

--- plugin/mutate/source/dextool/plugin/mutate/backend/report/html/trend.d
+++ plugin/mutate/source/dextool/plugin/mutate/backend/report/html/trend.d
@@ -23,53 +23,73 @@ import dextool.plugin.mutate.backend.report.analyzers : reportTrendByCodeChange,
 import dextool.plugin.mutate.backend.report.html.constants;
 import dextool.plugin.mutate.backend.type : Mutation;
 
+string randomColorHex(){
+    auto rng = Random(unpredictableSeed);
+    int colorValue;
+    string color = "#";
+    for(int i = 0; i < 6; i++){
+      colorValue = uniform(0, 15, rng);
+      if(colorValue < 10){
+        color ~= to!string(colorValue);
+      }else{
+        switch(colorValue){
+          case 10: color ~= "a";
+            break;
+          case 11: color ~= "b";
+            break;
+          case 12: color ~= "c";
+            break;
+          case 13: color ~= "d";
+            break;
+          case 14: color ~= "e";
+            break;
+          case 15: color ~= "f";
+            break;
+          default:
+            break;
+        }
+      }
+    }
+    return color;
+}
+
 void makeTrend(ref Database db, string tag, Element root, const(Mutation.Kind)[] kinds) @trusted {
     import std.datetime : SysTime;
     import dextool.plugin.mutate.backend.report.html.tmpl : TimeScalePointGraph;
 
     DashboardCss.h2(root.addChild(new Link(tag, null)).setAttribute("id", tag[1 .. $]), "Trend");
 
+    string[string] lineColor;
+    double[SysTime] totalScoreAtTime;
+    int[SysTime] fileCountAtTime;
     auto base = root.addChild("div");
-
     auto ts = TimeScalePointGraph("ScoreHistory");
 
     const history = reportMutationScoreHistory(db);
-    string color;
-    int colorValue;
-    auto rng = new Random(unpredictableSeed);
     double score;
+    string color;
 
+    //Add all the score histories to the graph
     if (history.data.length > 1){
       foreach(value; history.data){
-        color = "#";
-        for(int i = 0; i < 6; i++){
-          colorValue = uniform(0, 15, rng);
-          if(colorValue < 10){
-            color ~= to!string(colorValue);
-          }else{
-            switch(colorValue){
-              case 10: color ~= "a";
-                break;
-              case 11: color ~= "b";
-                break;
-              case 12: color ~= "c";
-                break;
-              case 13: color ~= "d";
-                break;
-              case 14: color ~= "e";
-                break;
-              case 15: color ~= "f";
-                break;
-              default:
-                break;
-            }
-          }
-        }
+        color = randomColorHex();
         score = rint(value.score.get * 1000)/1000;
-
+        totalScoreAtTime[value.timeStamp] += score;
+        fileCountAtTime[value.timeStamp] += 1;
+        lineColor[value.filePath] = color;
         ts.put(value.filePath, TimeScalePointGraph.Point(value.timeStamp, score));
         ts.setColor(value.filePath, color, color);
       }
+    }
+
+    //Generate Average
+    foreach(scoreAtTime; totalScoreAtTime.byKeyValue){
+      auto time = scoreAtTime.key;
+      score = scoreAtTime.value / fileCountAtTime[time];
+      score = rint(score * 1000)/1000;
+      ts.put("Average", TimeScalePointGraph.Point(time, score));
+      color = randomColorHex();
+      ts.setColor("Average", color, color);
     }
 
     ts.html(base, TimeScalePointGraph.Width(80));
