fd9128fe2bd52dbb642b5005d5a5411ccc03d432
Viktor Norgren <v.norgren01@gmail.com>
2022-06-22 12:45:23 +0200

    Removed redudant group

--- plugin/mutate/source/dextool/plugin/mutate/backend/database/package.d
+++ plugin/mutate/source/dextool/plugin/mutate/backend/database/package.d
@@ -278,7 +278,6 @@ struct Database {
             t0.mp_id = t1.id AND
             t1.file_id = t2.id AND
             t2.path = :path
-            GROUP BY t1.line, t3.checksum0 
             ORDER BY t1.offset_begin
             ", mutationTable, mutationPointTable, filesTable,
                 mutationStatusTable, kinds.map!(a => cast(int) a));
