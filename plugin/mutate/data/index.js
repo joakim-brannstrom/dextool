b99eeeee2c2e490be133015c1e85d59424e454cb
Axel Boström <56017794+axelbostrom@users.noreply.github.com>
2022-06-30 11:30:18 +0200

    mutate: fixed bad timezone (#1579)

--- plugin/mutate/data/index.js
+++ plugin/mutate/data/index.js
@@ -103,10 +103,10 @@ function setDocTime() {
 }
 
 function convertDate(date) {
-    return date.getUTCFullYear() + "/" +
-    ("0" + (date.getUTCMonth()+1)).slice(-2) + "/" +
-    ("0" + date.getUTCDate()).slice(-2) + " " +
-    ("0" + date.getUTCHours()).slice(-2) + ":" +
-    ("0" + date.getUTCMinutes()).slice(-2) + ":" +
-    ("0" + date.getUTCSeconds()).slice(-2);
+    return date.getFullYear() + "/" +
+    ("0" + (date.getMonth()+1)).slice(-2) + "/" +
+    ("0" + date.getDate()).slice(-2) + " " +
+    ("0" + date.getHours()).slice(-2) + ":" +
+    ("0" + date.getMinutes()).slice(-2) + ":" +
+    ("0" + date.getSeconds()).slice(-2);
 }
